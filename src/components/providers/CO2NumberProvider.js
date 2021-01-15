d4b1c29a9d2be3426b2807f1da62b5a68a34bb8d
florianpanchout <hi@florianpanchout.com>
2021-01-15 03:01:45 +0100

    Default CO2 Number

--- src/components/providers/CO2NumberProvider.js
+++ src/components/providers/CO2NumberProvider.js
@@ -3,7 +3,7 @@ import { useQueryParam, NumberParam, withDefault } from 'use-query-params'
 import CO2NumberContext from 'utils/CO2NumberContext'
 
 export default function CO2NumberProvider(props) {
-  const [CO2, setCO2] = useQueryParam('co2', withDefault(NumberParam, 1000))
+  const [CO2, setCO2] = useQueryParam('co2', withDefault(NumberParam, 2000))
 
   const [CO2EPopin, setCO2EPopin] = useState(false)
   return (
