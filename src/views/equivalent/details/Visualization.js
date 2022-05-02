1c0d8dbf7df4729f2dfca21c88d08eb865a8dfee
Florian <hi@florianpanchout.com>
2022-05-02 11:40:30 +0200

    Add visualizations

--- src/views/equivalent/details/Visualization.js
+++ src/views/equivalent/details/Visualization.js
@@ -3,11 +3,31 @@ import styled from 'styled-components'
 
 import Section from 'components/base/Section'
 import Boeuf from './visualization/Boeuf'
+import BoeufTemp from './visualization/BoeufTemp'
 import Smartphone from './visualization/Smartphone'
+import Eau from './visualization/Eau'
+import Voiture from './visualization/Voiture'
 
 const visualizations = {
   20689: <Boeuf />,
+  20683: <Boeuf />,
+  20684: <BoeufTemp />,
+  20685: <BoeufTemp />,
+  20686: <BoeufTemp />,
+  20688: <Boeuf />,
+  24283: <Eau />,
+  31000: <Eau />,
+  31001: <Eau />,
+  31002: <Eau />,
+  31003: <Eau />,
+  31004: <Eau />,
   27012: <Smartphone />,
+  27977: <Voiture />,
+  27976: <Voiture />,
+  27978: <Voiture />,
+  27979: <Voiture />,
+  206589: <Voiture />,
+  28081: <Voiture />,
 }
 const Wrapper = styled.div`
   margin-bottom: 4rem;
---
+++ src/views/equivalent/details/visualization/Voiture.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Button from 'components/base/Button'
+import Emoji from 'components/base/Emoji'
+
+const Equivalents = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1.5rem;
+`
+const Equivalent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: ${(props) => (props.large ? 19 : 5)}rem;
+`
+const Emojis = styled(Emoji)`
+  display: inline-flex;
+  flex-wrap: wrap;
+  gap: ${(props) => (props.small ? 0.125 : 0.375)}rem;
+  justify-content: center;
+  align-items: center;
+  min-height: 5.625rem;
+  margin-bottom: 0.25rem;
+  font-size: ${(props) => (props.small ? 0.875 : 2)}rem;
+  text-align: center;
+`
+const Label = styled.div`
+  text-align: center;
+  font-size: 0.875rem;
+  font-weight: 300;
+`
+const Equals = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+`
+const StyledButton = styled(Button)`
+  font-size: 0.875rem;
+`
+export default function Boeuf() {
+  return (
+    <>
+      <Equivalents>
+        <Equivalent>
+          <Emojis>🚗</Emojis>
+          <Label>
+            1 km
+            <br />
+            en voiture
+          </Label>
+        </Equivalent>
+        <Equals>=</Equals>
+        <Equivalent>
+          <Emojis>🚌🚌</Emojis>
+          <Label>
+            2 km
+            <br />
+            en bus
+          </Label>
+        </Equivalent>
+        <Equals>=</Equals>
+        <Equivalent large>
+          <Emojis small>
+            🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅🚅
+          </Emojis>
+          <Label>
+            112 km
+            <br />
+            en TGV
+          </Label>
+        </Equivalent>
+      </Equivalents>
+      <Button.Wrapper>
+        <StyledButton to='/categories/deplacement'>
+          Voir tous les modes de transport
+        </StyledButton>
+      </Button.Wrapper>
+    </>
+  )
+}
