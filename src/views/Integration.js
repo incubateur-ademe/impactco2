ca16904b2d1783e96e209022b6a8e78ee1b01bac
Florian <hi@florianpanchout.com>
2022-05-19 15:41:18 +0200

    Fix iframe

--- src/views/Integration.js
+++ src/views/Integration.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { useLocation } from '@reach/router'
 import IframeResizer from 'iframe-resizer-react'
 
 import Section from 'components/base/Section'
@@ -18,13 +17,12 @@ const StyledSectionContent = styled(Section.Content)`
 export default function Integration() {
   const [theme, setTheme] = useState('default')
 
-  const location = useLocation()
   return (
     <Section>
       <StyledSectionContent flex>
         <Configurator theme={theme} setTheme={setTheme} />
         <StyledIframeResizer
-          src={`${location.origin}/iframes/tuiles?theme=${theme}`}
+          src={`https://mcc-v2.netlify.app/iframes/tuiles?theme=${theme}`}
           allowfullscreen={true}
           webkitallowfullscreen={true}
           mozallowfullscreen={true}
