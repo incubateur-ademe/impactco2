7257050d2cfe604b7f77228c5505207598d21ff3
Florian <hi@florianpanchout.com>
2022-05-27 12:05:13 +0200

    Fix puppeteer

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,7 +28,7 @@ exports.onPostBuild = async () => {
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    headless: true,
   })
   const page = await browser.newPage()
   await page.setViewport({
