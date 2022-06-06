733fbf5d32ee1f6cc3656c17856dae780f0a9b8a
Florian <hi@florianpanchout.com>
2022-06-06 21:22:42 +0200

    Add emoji

--- src/views/equivalent/details/Value.js
+++ src/views/equivalent/details/Value.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import { formatTotal, formatNumber } from 'utils/formatters'
 import Section from 'components/base/Section'
+import Emoji from 'components/base/Emoji'
 
 const StyledSection = styled(Section)`
   margin-bottom: 0.75rem;
@@ -12,7 +13,6 @@ const StyledSection = styled(Section)`
 const Top = styled.div`
   position: relative;
   color: ${(props) => props.theme.colors.background};
-
   border-radius: 1rem 1rem 0 0;
   transition: padding 300ms ease-out, margin 300ms ease-out;
 `
@@ -38,10 +38,21 @@ const Big = styled.span`
     font-size: 1rem;
   }
 `
+const StyledEmoji = styled(Emoji)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4.5rem;
+  height: 4.5rem;
+  margin-top: 0.325rem;
+  font-size: 3rem;
+  background-color: ${(props) => props.theme.colors.background};
+  border-radius: 5.25rem;
+`
 export default function Value(props) {
   return (
     <StyledSection>
-      <Section.Content>
+      <Section.Content flex>
         <Top>
           <div>
             <Number>{formatNumber(formatTotal(props.equivalent))}</Number>{' '}
@@ -50,6 +61,7 @@ export default function Value(props) {
             </Unit>
           </div>
         </Top>
+        <StyledEmoji>{props.equivalent.emoji}</StyledEmoji>
       </Section.Content>
     </StyledSection>
   )
