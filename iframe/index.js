09bbb4ade03638ff0b99b44f299d0bf409e4abdd
Florian <hi@florianpanchout.com>
2022-06-10 12:03:34 +0200

    fix iframe url

--- iframe/index.js
+++ iframe/index.js
@@ -6,7 +6,7 @@ const type = script.dataset.type || 'tuiles'
 const search = script.dataset.search
 const source = window.location.href.toString()
 
-const src = `https://mcc-v2.netlify.app/iframe/${type}${search}&source=${source}`
+const src = `https://mcc-v2.netlify.app/iframes/${type}${search}&source=${source}`
 
 const iframe = document.createElement('iframe')
 
