43056d2b051522fc9cdfc20609cb4b750d6ce904
Florian <hi@florianpanchout.com>
2022-06-15 14:18:08 +0200

    Cleaning

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 export default function useDebounce(value, delay = 100) {
   useEffect(() => {
     const timer = setTimeout(() => {
-      console.log(value)
       window?._paq?.push(['trackEvent', 'Interaction', 'Search', value])
     }, delay)
     return () => {
