5f7c49d4c801554e40b28a7fbb50314e2b66dd8b
Florian <hi@florianpanchout.com>
2022-09-06 17:06:58 +0200

    Add fruits FE

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NextAdapter } from 'next-query-params'
-
 import { QueryParamProvider } from 'use-query-params'
+import { init } from '@socialgouv/matomo-next'
 
 import { GlobalStyle } from 'utils/styles'
 import fonts from 'utils/fonts.css'
@@ -13,6 +13,11 @@ import { DataProvider } from 'components/providers/DataProvider'
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient())
+
+  useEffect(() => {
+    init({ url: 'https://stats.data.gouv.fr', siteId: 156 })
+  }, [])
+
   return (
     <QueryParamProvider adapter={NextAdapter}>
       <QueryClientProvider client={queryClient}>
