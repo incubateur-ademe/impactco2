1d1c0ad4d1d8e115cbf50156a9487d74b25440d1
Florian <hi@florianpanchout.com>
2022-09-06 22:05:51 +0200

    Timer iframe

--- src/components/modals/SurveyModal.js
+++ src/components/modals/SurveyModal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import styled from 'styled-components'
 
 import ModalContext from 'components/providers/ModalProvider'
@@ -10,15 +10,26 @@ const StyledModal = styled(Modal)`
 export default function SurveyModal() {
   const { survey: open, setSurvey: setOpen } = useContext(ModalContext)
 
+  const [iframe, setIframe] = useState(false)
+  useEffect(() => {
+    const timer = setTimeout(() => setIframe(true), 2500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
   return (
     <StyledModal open={open} setOpen={setOpen}>
-      <iframe
-        title='enquete'
-        src='https://airtable.com/embed/shroHVp7DyXVn9YMf?backgroundColor=cyan'
-        frameBorder='0'
-        width='100%'
-        height='533'
-      ></iframe>
+      {open || iframe ? (
+        <iframe
+          title='enquete'
+          src='https://airtable.com/embed/shroHVp7DyXVn9YMf?backgroundColor=cyan'
+          frameBorder='0'
+          width='100%'
+          height='533'
+        ></iframe>
+      ) : null}
     </StyledModal>
   )
 }
