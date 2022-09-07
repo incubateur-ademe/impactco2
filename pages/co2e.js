3983aa73a03558004c8391d581ee4353f96effe1
Florian <hi@florianpanchout.com>
2022-09-07 16:19:55 +0200

    Fix meta

--- pages/co2e.js
+++ pages/co2e.js
@@ -5,7 +5,10 @@ import Tiles from 'components/misc/Tiles'
 
 export default function Co2e() {
   return (
-    <Web>
+    <Web
+      title={'Votre équivalent CO₂e ou CO₂e'}
+      description={`Calculez l'équivalent de votre émission CO₂e grâce au convertisseur CO2 Datagir et mesurez votre impact sur le climat`}
+    >
       <Tiles />
     </Web>
   )
