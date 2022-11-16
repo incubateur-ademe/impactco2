4a07d49df00c50db8c349374b2199c9b90d5d0e2
Florian <hi@florianpanchout.com>
2022-11-16 15:17:06 +0100

    Fix cors gmap

--- netlify/functions/callGMap.js
+++ netlify/functions/callGMap.js
@@ -8,11 +8,7 @@ exports.handler = function (event) {
     )
     .then((res) => ({
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTION',
-      },
+
       body: JSON.stringify(res.data),
     }))
 }
