830e561b935490c77a22d2840b67c1d511ce18a5
Florian <hi@florianpanchout.com>
2022-12-15 11:02:25 +0100

    Cleaning functions

--- netlify/functions/callGMap.js
+++ netlify/functions/callGMap.js
@@ -1,14 +1,12 @@
 const axios = require('axios')
 
 exports.handler = function (event) {
-  console.log(event)
   return axios
     .get(
       `https://maps.googleapis.com/maps/api/distancematrix/json?${event.rawQuery}&key=${process.env.GMAP_API_KEY}`
     )
     .then((res) => ({
       statusCode: 200,
-
       body: JSON.stringify(res.data),
     }))
 }
--- src/hooks/useAddress.js
+++ src/hooks/useAddress.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 
 const generateToken = () => (Math.random() + 1).toString(36).substring(2)
 
-let sessiontoken = generateToken()
-
-console.log('sessiontoken', sessiontoken)
 export function useSuggestions(search, focus) {
   return useQuery(
     ['search', search],
@@ -22,26 +19,6 @@ export function useSuggestions(search, focus) {
       refetchOnWindowFocus: false,
       staleTime: Infinity,
       enabled: focus,
-    }
-  )
-}
-export function useAddress(id) {
-  return useQuery(
-    ['address', id],
-    () =>
-      axios
-        .get(
-          `https://impactco2.fr/.netlify/functions/callGMapPlace?place_id=${id}&sessiontoken=${sessiontoken}`
-        )
-        .then((res) => {
-          sessiontoken = generateToken()
-          return res.data
-        }),
-    {
-      keepPreviousData: true,
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-      enabled: id ? true : false,
     }
   )
 }
