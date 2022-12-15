97bc4c52122ba60ba2376384581faea29bcb652b
Florian <hi@florianpanchout.com>
2022-12-15 07:32:12 +0100

    Fix layout

--- src/components/numerique/equivalent/LiseuseBookComparator.js
+++ src/components/numerique/equivalent/LiseuseBookComparator.js
@@ -7,17 +7,42 @@ import DataContext from 'components/providers/DataProvider'
 import ModalContext from 'components/providers/ModalProvider'
 import { Title } from 'components/misc/Visualization'
 import ButtonLink from 'components/base/ButtonLink'
+import MagicLink from 'components/base/MagicLink'
 
-const emailWeight = 0.0001 //ko
-
-const Text = styled.p`
-  margin-bottom: 0.25rem;
-  text-align: center;
+const Wrapper = styled.div`
+  height: 100%;
+  margin: 2rem 0;
 `
-const Result = styled.p`
-  margin: 1.5rem auto 2.5rem;
+const Question = styled.div`
+  margin-bottom: 2.5rem;
+`
+const Text = styled.p`
+  margin-bottom: 0;
   font-size: 1.125rem;
   text-align: center;
+
+  ${(props) => props.theme.mq.small} {
+    font-size: 1rem;
+  }
+`
+const Result = styled.p`
+  margin: 0 auto 2.5rem;
+  font-size: 1.125rem;
+  text-align: center;
+
+  ${(props) => props.theme.mq.small} {
+    font-size: 1rem;
+  }
+
+  strong {
+    display: block;
+    margin: 0.25rem auto;
+    font-size: 1.5rem;
+
+    ${(props) => props.theme.mq.small} {
+      font-size: 1.125rem;
+    }
+  }
 `
 const RangeWrapper = styled.div`
   display: flex;
@@ -25,7 +50,7 @@ const RangeWrapper = styled.div`
   width: 30rem;
   max-width: 100%;
   height: 2.5rem;
-  margin: 0 auto 0.25rem;
+  margin: 0.5rem auto;
 `
 const Track = styled.div`
   position: relative;
@@ -47,9 +72,10 @@ const Thumb = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  width: 6.5rem;
+  width: 7.5rem;
   height: 2.5rem;
   padding: 0 1rem;
+  font-size: 1.25rem;
   font-weight: 700;
   text-align: center;
   color: ${(props) => props.theme.colors.background};
@@ -59,6 +85,11 @@ const Thumb = styled.div`
   &:focus {
     outline: none;
     box-shadow: 0 0 0 0.125rem ${(props) => props.theme.colors.mainLight};
+  }
+
+  ${(props) => props.theme.mq.small} {
+    width: 6.5rem;
+    font-size: 1rem;
   }
 `
 export default function LiseuseBookComparator() {
@@ -80,48 +111,62 @@ export default function LiseuseBookComparator() {
   const [numBookPerYear, setNumBookPerYear] = useState(10)
 
   return (
-    <>
-      <Title>Devriez-vous acheter une liseuse ?</Title>
-      <Text>Je lis en moyenne</Text>
-      <RangeWrapper onMouseDown={(e) => e.stopPropagation()}>
-        <Range
-          step={1}
-          min={0}
-          max={50}
-          values={[numBookPerYear]}
-          onChange={(values) => {
-            setNumBookPerYear(values[0])
-          }}
-          renderTrack={({ props, children }) => (
-            <Track {...props}>{children}</Track>
-          )}
-          renderThumb={({ props }) => (
-            <Thumb {...props} aria-label='Nombre de livres'>
-              {numBookPerYear} livre{numBookPerYear > 1 ? 's' : ''}
-            </Thumb>
-          )}
-        />
-      </RangeWrapper>
-      <Text>par an</Text>
-      {numBookPerYear ? (
-        <Result>
-          Il faudrait que j'utilise ma liseuse pendant au moins{' '}
-          <strong>
-            {Math.ceil(
-              formatTotal(liseuse) / (formatTotal(livre) * numBookPerYear)
-            )}{' '}
-            ans
+    <Wrapper>
+      <Question>
+        <Text>Je lis en moyenne</Text>
+        <RangeWrapper
+          onMouseDown={(e) => e.stopPropagation()}
+          onTouchStart={(e) => e.stopPropagation()}
+        >
+          <Range
+            step={1}
+            min={0}
+            max={50}
+            values={[numBookPerYear]}
+            onChange={(values) => {
+              setNumBookPerYear(values[0])
+            }}
+            renderTrack={({ props, children }) => (
+              <Track {...props}>{children}</Track>
+            )}
+            renderThumb={({ props }) => (
+              <Thumb {...props} aria-label='Nombre de livres'>
+                {numBookPerYear} livre{numBookPerYear > 1 ? 's' : ''}
+              </Thumb>
+            )}
+          />
+        </RangeWrapper>
+        <Text>par an</Text>
+      </Question>
+      <div>
+        {numBookPerYear ? (
+          <Result>
+            Il faudrait que j'utilise ma liseuse pendant au moins{' '}
+            <strong>
+              {Math.ceil(
+                formatTotal(liseuse) / (formatTotal(livre) * numBookPerYear)
+              )}{' '}
+              ans
+              <br />
+            </strong>{' '}
+            avant qu'elle émette moins de{' '}
+            <ButtonLink onClick={() => setCo2e(true)}>
+              CO<sub>2</sub>e
+            </ButtonLink>{' '}
+            que l'équivalent en livres papier.
+          </Result>
+        ) : (
+          <Result>
+            Vous ne devriez probablement pas acheter de liseuse.
             <br />
-          </strong>{' '}
-          avant qu'elle émette moins de{' '}
-          <ButtonLink onClick={() => setCo2e(true)}>
-            CO<sub>2</sub>e
-          </ButtonLink>{' '}
-          que l'équivalent en livres papier.
-        </Result>
-      ) : (
-        <Result>Vous ne devriez probablement pas acheter de liseuse.</Result>
-      )}
-    </>
+            Si vous en possédez déjà une, vous pouvez lui donner une seconde
+            vie. Découvrez comment avec notre site{' '}
+            <MagicLink to='https://longuevieauxobjets.gouv.fr/'>
+              Longue Vie Aux Objets
+            </MagicLink>
+          </Result>
+        )}
+      </div>
+    </Wrapper>
   )
 }
