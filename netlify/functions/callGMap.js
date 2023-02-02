92a864af4eee3cc9c34eabd172b082c491701664
Florian <hi@florianpanchout.com>
2023-02-02 16:08:41 +0100

    fix: log referrer on gmap call

--- netlify/functions/callGMap.js
+++ netlify/functions/callGMap.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 
 exports.handler = function (event) {
+  console.log(event.headers.referrer)
   if (
     event.headers.referrer.includes('impactco2.fr') ||
     event.headers.referrer.includes('monimpacttransport.fr')
