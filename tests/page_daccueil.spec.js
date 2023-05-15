7433f0d5704504ea57303467827183117f482047
David Boureau <bdavidxyz@gmail.com>
2023-05-15 19:43:02 +0200

    feat: more readable specs

--- tests/page_daccueil.spec.js
+++ tests/page_daccueil.spec.js
@@ -2,6 +2,7 @@
 const { test, expect } = require('@playwright/test')
 
 test("Le titre de la page d'accueil est bien renseigné", async ({ page }) => {
+  // Ouverture de la page d'accueil dans le navigateur
   await page.goto('/')
 
   // Le titre de la page est bien renseigné
@@ -11,8 +12,9 @@ test("Le titre de la page d'accueil est bien renseigné", async ({ page }) => {
 test("La barre de recherche de la page d'accueil suggère (ou pas) des résultats", async ({
   page,
 }) => {
-  // Chargement de la page
+  // Ouverture de la page d'accueil dans le navigateur
   await page.goto('/')
+
   // Pas de suggestion au départ
   expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
 
@@ -23,36 +25,53 @@ test("La barre de recherche de la page d'accueil suggère (ou pas) des résultat
 
   // Entrer un seul caractère
   await page.keyboard.type('t')
+
   // Toujours pas de suggestion
   expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
 
-  // Entrer un caractère bizarre, un message explicite l'absence de résultat
+  // Entrer un caractère bizarre
   await page.keyboard.type('w')
+
+  // Toujours pas de suggestion...
   expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
+  // ...Et un message "non trouvé" s'affiche
   expect(await page.getByTestId('notfound').count()).toEqual(1)
 
-  // Entrer trois caractères
+  // Supprimer un caractère
   await page.keyboard.press('Backspace')
+  // Entrer un nouveau caractère...
   await page.keyboard.type('r')
+  // Et un troisième
   await page.keyboard.type('a')
-
-  // Il y a bien des suggestions
-  expect(await page.getByTitle('simple suggestion').count()).toBeGreaterThan(0)
 })
 
 test("Le clic sur la suggestion d'un résultat de la page d'accueil redirige vers le détail", async ({
   page,
 }) => {
+  // Ouverture de la page d'accueil dans le navigateur
   await page.goto('/')
+
+  // Click sur la barre de recherche
   await page
     .getByRole('textbox', { name: 'Entrez un objet, un geste...' })
     .click({ force: true })
+
+  // L'utilisateur entre un texte en rapport avec les transports
   await page.keyboard.type('t')
+  // Entrer un nouveau caractère...
   await page.keyboard.type('r')
+  // Et un troisième
   await page.keyboard.type('a')
-  await page.keyboard.type('i')
-  await page.keyboard.type('n')
-  page.keyboard.press('Tab')
-  page.keyboard.press('Enter')
+
+  // Il y a bien des suggestions
+  expect(await page.getByTitle('simple suggestion').count()).toBeGreaterThan(0)
+
+  // L'utilisateur appui sur la touche "Tab"
+  await page.keyboard.press('Tab')
+
+  // L'utilisateur appui sur la touche "Entrée"
+  await page.keyboard.press('Enter')
+
+  // L'utilisateur est redirigé vers une URL qui concerne les transports
   await expect(page).toHaveURL(/.*transport/)
 })
