9138923d0eb4796ce61d8243fb2cb7aaf97fb8b3
David Boureau <bdavidxyz@gmail.com>
2023-05-15 20:15:26 +0200

    refactor: readable lines

--- tests/page_daccueil.spec.js
+++ tests/page_daccueil.spec.js
@@ -15,8 +15,17 @@ test("La barre de recherche de la page d'accueil ne suggère pas de résultats",
   // Ouverture de la page d'accueil dans le navigateur
   await page.goto('/')
 
+  // Comptage du nombre de message affichant "non trouvé"
+  let nb_of_notfound = await page.getByTestId('notfound').count()
+
+  // Pas de "non trouvé" explicite au départ
+  expect(nb_of_notfound).toEqual(0)
+
+  // Comptage des suggestions
+  let nb_of_suggestions = await page.getByTitle('simple suggestion').count()
+
   // Pas de suggestion au départ
-  expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
+  expect(nb_of_suggestions).toEqual(0)
 
   // Click sur la barre de recherche
   await page
@@ -26,16 +35,26 @@ test("La barre de recherche de la page d'accueil ne suggère pas de résultats",
   // Entrer un seul caractère
   await page.keyboard.type('t')
 
+  // Nouveau comptage des suggestions
+  nb_of_suggestions = await page.getByTitle('simple suggestion').count()
+
   // Toujours pas de suggestion
-  expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
+  expect(nb_of_suggestions).toEqual(0)
 
   // Entrer un caractère bizarre
   await page.keyboard.type('w')
 
-  // Toujours pas de suggestion...
-  expect(await page.getByTitle('simple suggestion').count()).toEqual(0)
-  // ...Et un message "non trouvé" s'affiche
-  expect(await page.getByTestId('notfound').count()).toEqual(1)
+  // Encore un comptage des suggestions
+  nb_of_suggestions = await page.getByTitle('simple suggestion').count()
+
+  // Toujours pas de suggestion
+  expect(nb_of_suggestions).toEqual(0)
+
+  // Comptage du nombre de message affichant "non trouvé"
+  nb_of_notfound = await page.getByTestId('notfound').count()
+
+  // Cette fois-ci, un message "non trouvé" s'affiche
+  expect(nb_of_notfound).toEqual(1)
 })
 
 test("La barre de recherche de la page d'accueil suggère des résultats", async ({
@@ -51,13 +70,18 @@ test("La barre de recherche de la page d'accueil suggère des résultats", async
 
   // L'utilisateur entre un texte en rapport avec les transports
   await page.keyboard.type('t')
+
   // Entrer un nouveau caractère...
   await page.keyboard.type('r')
+
   // Et un troisième
   await page.keyboard.type('a')
 
+  // Comptage des suggestions
+  let nb_of_suggestions = await page.getByTitle('simple suggestion').count()
+
   // Il y a bien des suggestions
-  expect(await page.getByTitle('simple suggestion').count()).toBeGreaterThan(0)
+  expect(nb_of_suggestions).toBeGreaterThan(0)
 
   // L'utilisateur appui sur la touche "Tab"
   await page.keyboard.press('Tab')
