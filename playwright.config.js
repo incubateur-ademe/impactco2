d6baf673922dd72baa181756bf364f9c3ec60126
David Boureau <bdavidxyz@gmail.com>
2023-05-16 14:51:18 +0200

    feat: reuseExistingServer

--- playwright.config.js
+++ playwright.config.js
@@ -81,6 +81,6 @@ module.exports = defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 })
