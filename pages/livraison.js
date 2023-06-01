37d014e3bed6d715e9331fe8af573ffb6dfec1a6
David Boureau <bdavidxyz@gmail.com>
2023-06-01 17:32:35 +0200

    design: styled H1

--- pages/livraison.js
+++ pages/livraison.js
@@ -1,14 +1,23 @@
 import categories from 'data/categories.json'
+import styled from 'styled-components'
 
 import Section2 from 'components/base/Section2'
+
+const H1Title = styled.h1`
+  margin-top: 0;
+`
+const MainColorSpan = styled.span`
+  color: ${(props) => props.theme.colors.main};
+`
 
 export default function Livraison() {
   return (
     <Section2>
       <Section2.InnerMargin>
-        <h1>
-          Mesurer l'impact carbone de la <span>livraison de colis</span>
-        </h1>
+        <H1Title>
+          Mesurer l'impact carbone de la{' '}
+          <MainColorSpan>livraison de colis</MainColorSpan>
+        </H1Title>
       </Section2.InnerMargin>
     </Section2>
   )
