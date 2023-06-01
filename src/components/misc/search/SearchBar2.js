63f0689a8269e6555be21b956bca1c8a1c9bfdfb
David Boureau <bdavidxyz@gmail.com>
2023-06-01 17:11:25 +0200

    feat: search unbugged

--- src/components/misc/search/SearchBar2.js
+++ src/components/misc/search/SearchBar2.js
@@ -9,23 +9,28 @@ import Suggestions from './searchBar/Suggestions'
 import TextInput from './searchBar/TextInput'
 
 const Wrapper = styled.form`
+  height: 100%;
+  > input {
+    height: 100%;
+  }
   ${(props) => props.theme.mq[props.hideon]} {
     display: none;
   }
-  background-color: ${(props) =>
-    props.focus ? props.theme.colors.background : 'transparent'};
-  border: 0.125rem solid ${(props) => props.theme.colors.main};
+  border:  ${(props) =>
+    props.focus ? `0.125rem solid ${props.theme.colors.main}` : 'none'};
   border-radius: ${(props) =>
     props.home || props.focus ? ' 0.625em' : '4rem'};
   box-shadow: ${(props) =>
     props.focus ? '-0.25rem 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.05)' : 'none'};
+  cursor: pointer;
   left: ${(props) => (props.home ? 0 : 'auto')};
-  max-width: 12rem;
+  max-width: ${(props) => (props.focus ? 'calc(100vw - 1.5rem)' : '2.75rem')};
+  margin-right: ${(props) => (props.focus ? '1rem' : '0')};
   right: 0;
   top: 0;
   transition: box-shadow 300ms ease-out, background-color 300ms ease-out;
   width: ${(props) =>
-    props.home ? 'auto' : props.focus ? 'calc(100vw - 1.5rem)' : '2.375rem'};
+    props.home ? 'auto' : props.focus ? 'calc(100vw - 1.5rem)' : '1rem'}
   z-index: 100;
 `
 
