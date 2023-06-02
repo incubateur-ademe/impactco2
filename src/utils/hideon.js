155764e254289f8c24cc511e51ffd1004eb6bf0e
David Boureau <bdavidxyz@gmail.com>
2023-06-02 11:02:55 +0200

    feat: hideon def

--- src/utils/hideon.js
+++ src/utils/hideon.js
@@ -1,5 +1,18 @@
 import { css } from 'styled-components'
 
+/**
+ *
+ * Allow to hide a component below a given media query.
+ *
+ * Usage : <MySearchBar hideon={'large'} />
+ *
+ * Definition:
+ *
+ * import hideon from 'utils/hideon.js'
+ * const Wrapper = styled.form`${hideon}`
+ * export default function MySearchBar(props) {return (<Wrapper hideon={props.hideon}></Wrapper>
+ *
+ */
 export const hideon = css`
   ${(props) => {
     if (!props.hideon) {
