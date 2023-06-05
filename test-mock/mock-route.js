ba88420d8f13caaf6a2149eb7e71404aed81a00c
David Boureau <bdavidxyz@gmail.com>
2023-06-05 15:41:14 +0200

    feat: mocked hotjar

--- test-mock/mock-route.js
+++ test-mock/mock-route.js
@@ -1,4 +1,13 @@
-export const mockRoutes = async (page) =>
+export const mockRoutes = async (page) => {
+  await page.route(/hotjar/, async (route) => {
+    await route.fulfill({
+      status: 200,
+      headers: {
+        Etag: 'mocked, because it was run in a E2E environment',
+      },
+    })
+  })
+
   await page.route(
     'https://deploy-preview-1895--ecolab-data.netlify.app/co2-model.FR-lang.fr.json',
     async (route) => {
@@ -1355,3 +1364,4 @@ export const mockRoutes = async (page) =>
       })
     }
   )
+}
