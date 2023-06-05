295ec60bc14b86917abc816c680717b82da21971
David Boureau <bdavidxyz@gmail.com>
2023-06-05 11:39:24 +0200

    design: colors ok

--- src/components/livraison/Resultat.js
+++ src/components/livraison/Resultat.js
@@ -18,16 +18,24 @@ const Subexplain = styled.span`
 `
 
 const Wrapper = styled.div`
+  background-color: #457be7;
+  border-radius: 16px;
+  color: white;
   display: grid;
   grid-template-columns: 30px 1fr;
   margin-top: 1rem;
-  width: 220px;
+  max-width: 350px;
   > .item1 {
     align-items: center;
     display: flex;
     justify-content: end;
     margin-right: 4px;
     margin-top: 7px;
+  }
+  > .item4 {
+    color: #aec8fc;
+    margin-bottom: 1rem;
+    margin-top: -5px;
   }
 `
 
@@ -48,8 +56,7 @@ export default function Resultat(props) {
           xmlns='http://www.w3.org/2000/svg'
           width='16'
           height='16'
-          fill='currentColor'
-          class='bi bi-tree'
+          fill='#AEC8FC'
           viewBox='0 0 16 16'
         >
           <path d='M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5zM6.437 4.758A.5.5 0 0 0 6 4.5h-.066L8 1.401 10.066 4.5H10a.5.5 0 0 0-.424.765L11.598 8.5H11.5a.5.5 0 0 0-.447.724L12.69 12.5H3.309l1.638-3.276A.5.5 0 0 0 4.5 8.5h-.098l2.022-3.235a.5.5 0 0 0 .013-.507z' />
