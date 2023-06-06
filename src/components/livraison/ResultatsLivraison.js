50a29a8abb0e22d953b2e6ddc8cd4db793261004
David Boureau <bdavidxyz@gmail.com>
2023-06-06 14:50:21 +0200

    feat: added one line at 120 len

--- src/components/livraison/ResultatsLivraison.js
+++ src/components/livraison/ResultatsLivraison.js
@@ -1,11 +1,8 @@
-import React, { useContext, useMemo } from 'react'
-import styled from 'styled-components'
-
-import DataContext from 'components/providers/DataProvider'
-
-import Tile2 from 'components/misc/tiles/Tile2'
-
-import ResultatLivraison from './ResultatLivraison'
+import ResultatLivraison from "./ResultatLivraison";
+import LivraisonEq from "components/misc/tiles/LivraisonEq";
+import DataContext from "components/providers/DataProvider";
+import React, { useContext, useMemo } from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   background-color: whitesmoke;
@@ -19,34 +16,25 @@ const Wrapper = styled.div`
     grid-template-columns: 1fr;
     grid-template-rows: repeat(4, 1fr);
   }
-`
+`;
 
 export default function ResultatsLivraison(props) {
-  const { equivalents } = useContext(DataContext)
+  const { equivalents } = useContext(DataContext);
   const equivalentsToShow = useMemo(
     () =>
       equivalents.filter((equivalent) =>
-        ['voiturethermique', 'repasavecduboeuf', 'streamingvideo'].includes(
-          equivalent.slug
-        )
+        ["voiturethermique", "repasavecduboeuf", "streamingvideo"].includes(equivalent.slug)
       ),
     [equivalents]
-  )
+  );
   return (
     <Wrapper>
-      <div className='item item1'>
+      <div className="item item1">
         <ResultatLivraison co2eq={props.co2eq} />
       </div>
       {equivalentsToShow.map((equivalent) => (
-        <Tile2
-          key={equivalent.slug}
-          equivalent={equivalent}
-          weight={(props.co2eq / 1000) * props.freqMultBy}
-          equivalentPage
-          reference
-          noAnimation
-        />
+        <LivraisonEq key={equivalent.slug} equivalent={equivalent} weight={(props.co2eq / 1000) * props.freqMultBy} />
       ))}
     </Wrapper>
-  )
+  );
 }
