f73ebb58ac2957fe5f5233fc6848ad4fe5e55ecb
David Boureau <bdavidxyz@gmail.com>
2023-06-06 15:29:11 +0200

    feat: trying styled component outside

--- src/components/misc/tiles/LivraisonEq.js
+++ src/components/misc/tiles/LivraisonEq.js
@@ -1,28 +1,26 @@
+import { Wrapper, EmojiWrapper, Title, Number, OfWhat } from "./LivraisonEq.styled";
 import Emoji from "components/base/Emoji";
-import styled from "styled-components";
 import { formatName, formatNumber, formatTotalByMultiplier } from "utils/formatters";
 
-const Wrapper = styled.div``;
-
-const EmojiWrapper = styled.div``;
-const Title = styled.p``;
-const Number = styled.span``;
-const Name = styled.span``;
-
 export default function LivraisonEq(props) {
+  const fullSentence =
+    formatNumber(props.weight / formatTotalByMultiplier(props.equivalent)) +
+    " " +
+    formatName(
+      (props.equivalent.prefix || "") + props.equivalent.name,
+      props.weight / formatTotalByMultiplier(props.equivalent)
+    );
+  const twoFirstWordsOnly = (sentence) => sentence.split(" ").slice(0, 2).join(" ");
+  const removeFirstTwoWords = (sentence) => sentence.split(" ").slice(2).join(" ");
+
   return (
     <Wrapper background={props.background}>
       <EmojiWrapper>
         <Emoji>{props.equivalent.emoji}</Emoji>
       </EmojiWrapper>
       <Title>
-        <Number>{formatNumber(props.weight / formatTotalByMultiplier(props.equivalent))}</Number>
-        <Name>
-          {formatName(
-            (props.equivalent.prefix || "") + props.equivalent.name,
-            props.weight / formatTotalByMultiplier(props.equivalent)
-          )}
-        </Name>
+        <Number>{twoFirstWordsOnly(fullSentence)}</Number>
+        <OfWhat>{removeFirstTwoWords(fullSentence)}</OfWhat>
       </Title>
     </Wrapper>
   );
---
+++ src/components/misc/tiles/LivraisonEq.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  align-content: center;
+  display: grid;
+  grid-template-columns: 30px 1fr;
+  grid-template-rows: 1fr;
+  justify-content: center;
+`;
+
+export const EmojiWrapper = styled.div``;
+
+export const Title = styled.p``;
+
+export const Number = styled.span``;
+
+export const OfWhat = styled.span``;
