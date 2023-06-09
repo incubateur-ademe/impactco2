4f1f506f52d12a79b2560f83aff7a6d7278f02bf
David Boureau <bdavidxyz@gmail.com>
2023-06-09 11:32:48 +0200

    refactor: styled modal

--- src/components/base/Modal2.js
+++ src/components/base/Modal2.js
@@ -39,22 +39,12 @@ const Content = styled.div`
   visibility: ${(props) => (props.open ? "visible" : "hidden")};
   width: ${(props) => props.width || "40em"};
 `;
-const ButtonClose = styled.div`
-  cursor: pointer;
-  font-size: 2rem;
-  font-weight: bold;
-  line-height: 0.5;
-  position: absolute;
-  right: 0.5em;
-  top: 0.5em;
-  transform: rotate(45deg);
-  z-index: 12;
-`;
+const ButtonClose = styled.div``;
 const Scroll = styled.div`
   overflow-y: auto;
   padding: 2rem 1.5rem;
 `;
-export default function Modal(props) {
+export default function Modal2(props) {
   return (
     <Wrapper open={props.open}>
       <Background open={props.open} onClick={() => props.setOpen(false)} />
@@ -65,9 +55,32 @@ export default function Modal(props) {
         backgroundColor={props.backgroundColor}
         noAnimation={props.noAnimation}
       >
-        <ButtonClose onClick={() => props.setOpen(false)}>*</ButtonClose>
+        <Header>
+          {props.getTitle ? props.getTitle() : "Titre"}
+          <ButtonClose onClick={() => props.setOpen(false)}>
+            Fermer
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-x-lg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+          </ButtonClose>
+        </Header>
+
         <Scroll className={props.className}>{props.children}</Scroll>
       </Content>
     </Wrapper>
   );
 }
+
+const Header = styled.div`
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  padding: 1.5rem;
+`;
