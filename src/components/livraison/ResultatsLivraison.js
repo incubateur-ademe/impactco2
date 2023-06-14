b57bc6ade823749603dd2020bc65d32d2cbf669d
David Boureau <bdavidxyz@gmail.com>
2023-06-14 12:32:57 +0200

    refactor: par livraison seulement

--- src/components/livraison/ResultatsLivraison.js
+++ src/components/livraison/ResultatsLivraison.js
@@ -4,21 +4,8 @@ import DataContext from "components/providers/DataProvider";
 import React, { useContext, useMemo } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  border: 1px solid #457be7;
-  border-radius: 16px;
-  display: grid;
-  /* grid-template-columns: 22% 4% 22% 22% 22%; */
-  grid-template-columns: repeat(4, 1fr);
-  margin-top: 1rem;
-  ${(props) => props.theme.mq.xlarge} {
-    grid-template-columns: 1fr;
-    /* grid-template-rows: 22% 4% 22% 22% 22%; */
-    grid-template-rows: repeat(4, 1fr);
-  }
-`;
-
 export default function ResultatsLivraison(props) {
+  console.log("props.co2eq", props.co2eq);
   const { equivalents } = useContext(DataContext);
   const equivalentsToShow = useMemo(
     () =>
@@ -40,3 +27,17 @@ export default function ResultatsLivraison(props) {
     </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  border: 1px solid #457be7;
+  border-radius: 16px;
+  display: grid;
+  /* grid-template-columns: 22% 4% 22% 22% 22%; */
+  grid-template-columns: repeat(4, 1fr);
+  margin-top: 1rem;
+  ${(props) => props.theme.mq.xlarge} {
+    grid-template-columns: 1fr;
+    /* grid-template-rows: 22% 4% 22% 22% 22%; */
+    grid-template-rows: repeat(4, 1fr);
+  }
+`;
