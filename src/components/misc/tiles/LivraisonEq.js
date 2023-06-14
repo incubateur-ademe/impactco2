495901f97fa484c620e990c7c59e68b4bbf55dcd
David Boureau <bdavidxyz@gmail.com>
2023-06-14 12:11:22 +0200

    style: better margin

--- src/components/misc/tiles/LivraisonEq.js
+++ src/components/misc/tiles/LivraisonEq.js
@@ -1,5 +1,5 @@
-import { Wrapper, EmojiWrapper, Number, OfWhat } from "./LivraisonEq.styled";
 import Emoji from "components/base/Emoji";
+import styled from "styled-components";
 import { fullSentenceFormat } from "utils/formatters";
 
 const first2WordsOnly = (sentence) => sentence.split(" ").slice(0, 2).join(" ");
@@ -17,3 +17,35 @@ export default function LivraisonEq(props) {
     </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  align-content: center;
+  display: grid;
+  grid-template-columns: 40px 1fr;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const EmojiWrapper = styled.div`
+  > span > img.emoji {
+    height: 32px;
+    width: 32px;
+  }
+`;
+
+const Number = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 0em;
+  line-height: 32px;
+`;
+
+const OfWhat = styled.div`
+  color: #746770;
+  font-size: 14px;
+  font-weight: 400;
+  letter-spacing: 0em;
+  line-height: 16px;
+  margin-top: -8px;
+`;
