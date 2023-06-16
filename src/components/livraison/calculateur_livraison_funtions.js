c7bfb6123dbd0f78ce087d7b40c39918d7c6699e
David Boureau <bdavidxyz@gmail.com>
2023-06-16 11:06:43 +0200

    refactor: simplified calculation

--- src/components/livraison/calculateur_livraison_funtions.js
+++ src/components/livraison/calculateur_livraison_funtions.js
@@ -2,41 +2,24 @@ export const calculateResultFunction = (values, produits, retraits, relays, engi
   let produitCode = produits.find((p) => p.uid === values.produit).publicode;
   let retraitCode = retraits.find((r) => r.uid === values.retrait).publicode;
   let relayCode = relays.find((r) => r.uid === values.relay).publicode;
-  console.log("relayCode", relayCode);
 
-  const produitsEtRetraits = {
+  const baseCodes = getPublicodes(produitCode, retraitCode, relayCode, "0");
+  engine.setSituation(baseCodes);
+  let zeroKmCO2 = engine.evaluate("livraison colis").nodeValue;
+
+  const kmCodes = getPublicodes(produitCode, retraitCode, relayCode, values.km);
+  engine.setSituation(kmCodes);
+  let actualKmCO2 = engine.evaluate("livraison colis").nodeValue;
+
+  setDiffs({ ...diffs, diffKm0: actualKmCO2 - zeroKmCO2 });
+  setCO2eq(actualKmCO2);
+};
+
+const getPublicodes = (produitCode, retraitCode, relayCode, km) => {
+  return {
     "livraison colis . informations . catégorie": `'${produitCode}'`,
     "livraison colis . scénario": `'${retraitCode}'`,
     "livraison colis . déplacement consommateur . mode de déplacement": `'${relayCode}'`,
+    "livraison colis . déplacement consommateur . distance": `'${km}'`,
   };
-
-  let newSituation0km = {
-    ...produitsEtRetraits,
-    "livraison colis . déplacement consommateur . distance": `'0'`,
-  };
-
-  let newSituationWithKm = {
-    ...produitsEtRetraits,
-    "livraison colis . déplacement consommateur . distance": `'${values.km}'`,
-  };
-
-  engine.setSituation(newSituation0km);
-  let zeroKmCO2 = engine.evaluate("livraison colis").nodeValue;
-  console.log("zeroKmCO2", zeroKmCO2);
-
-  engine.setSituation(newSituationWithKm);
-  let actualKmCO2 = engine.evaluate("livraison colis").nodeValue;
-  console.log("actualKmCO2", actualKmCO2);
-
-  let newSituationRelay = {
-    ...produitsEtRetraits,
-    "livraison colis . déplacement consommateur . distance": `'${values.km}'`,
-    "livraison colis . déplacement consommateur . mode de déplacement": `'${relayCode}'`,
-  };
-  engine.setSituation(newSituationRelay);
-  let actualRelay = engine.evaluate("livraison colis").nodeValue;
-  console.log("actualRelay", actualRelay);
-
-  setDiffs({ ...diffs, diffKm0: actualKmCO2 - zeroKmCO2 });
-  setCO2eq(engine.evaluate("livraison colis").nodeValue);
 };
