7131df8f717cc8b6c5912e6cbaa7a3f48c223703
David Boureau <bdavidxyz@gmail.com>
2023-06-16 15:03:28 +0200

    refactor: remove pushed footer

--- src/components/layout/Web2.js
+++ src/components/layout/Web2.js
@@ -1,40 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
+import BreadCrumb2 from "./web/BreadCrumb2";
+import Footer2 from "./web/Footer2";
+import Header2 from "./web/Header2";
+import Seo from "./web/Seo";
+import useInteraction from "hooks/useInteraction";
+import React from "react";
+import styled from "styled-components";
 
-import useInteraction from 'hooks/useInteraction'
-
-import BreadCrumb2 from './web/BreadCrumb2'
-import Footer2 from './web/Footer2'
-import Header2 from './web/Header2'
-import Seo from './web/Seo'
-
-const VerticalContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`
-const ToTheBottom = styled.div`
-  margin-top: auto;
-`
+const VerticalContainer = styled.div``;
 
 export default function Web2(props) {
-  useInteraction()
+  useInteraction();
 
   return (
     <>
       <VerticalContainer>
-        <Seo
-          title={props.title}
-          description={props.description}
-          image={props.image}
-        />
+        <Seo title={props.title} description={props.description} image={props.image} />
         <Header2 />
         <BreadCrumb2 breadcrumb={props.breadcrumb} />
         {props.children}
-        <ToTheBottom>
-          <Footer2 />
-        </ToTheBottom>
+        <Footer2 />
       </VerticalContainer>
     </>
-  )
+  );
 }
