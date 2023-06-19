98cfd6e19fdd105a0e61d6350d0126f7be62a061
David Boureau <bdavidxyz@gmail.com>
2023-06-19 15:30:45 +0200

    feat: building FruitSearch

--- src/components/modals/FruitSearch.js
+++ src/components/modals/FruitSearch.js
@@ -1,25 +1,15 @@
 import Fuse from "../../../node_modules/fuse.js/dist/fuse.basic.esm.min.js";
 import Equivalent from "./tilesModal/Equivalent";
-import Modal3 from "components/base/Modal3";
 import TextInput from "components/base/TextInput";
 import DataContext from "components/providers/DataProvider";
-import ModalContext from "components/providers/ModalProvider";
 import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 
-const getTitle = () => {
-  return (
-    <Title>
-      Choisir <GreenText>d&apos;autres équivalences</GreenText>
-    </Title>
-  );
-};
-
-export default function EqModal3() {
-  const { eqv: open, setEqv: setOpen } = useContext(ModalContext);
-
+export default function FruitSearch() {
   /** */
   const { equivalents, tiles, setTiles } = useContext(DataContext);
+
+  const FRUIT_CATEGORY = 9;
 
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
@@ -63,10 +53,7 @@ export default function EqModal3() {
   /** */
 
   return (
-    <Modal3 open={open} setOpen={setOpen} getTitle={getTitle} width="45rem">
-      <Intro>Sélectionnez (ou désélectionnez) des équivalents pour créer votre infographie personnalisée.</Intro>
-      <H2Title>Fruits et légumes</H2Title>
-
+    <>
       <SearchInput
         value={search}
         onChange={({ value }) => setSearch(value)}
@@ -75,7 +62,7 @@ export default function EqModal3() {
       {open && (
         <Equivalents>
           {results
-            .filter((r) => r.item.category === 9)
+            .filter((r) => r.item.category === FRUIT_CATEGORY)
             .slice(0, 3)
             .map(({ item }) => (
               <Equivalent
@@ -92,35 +79,9 @@ export default function EqModal3() {
             ))}
         </Equivalents>
       )}
-
-      <H2Title>Usages du numérique</H2Title>
-    </Modal3>
+    </>
   );
 }
-
-const Title = styled.h2`
-  font-size: 22px;
-  margin: 1rem 0;
-`;
-
-const GreenText = styled.span`
-  color: #1c9b93;
-`;
-
-const Intro = styled.div`
-  font-size: 16px;
-  font-weight: 300;
-  letter-spacing: 0em;
-  line-height: 24px;
-`;
-
-const H2Title = styled.div`
-  color: #1c9b93;
-  font-size: 16px;
-  font-weight: 700;
-  letter-spacing: 0em;
-  line-height: 24px;
-`;
 
 const SearchInput = styled(TextInput)`
   margin: 0.5rem;
