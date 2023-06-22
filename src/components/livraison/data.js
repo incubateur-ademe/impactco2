4185d8f459f45ed5004cede0cea270eac85488f9
David Boureau <bdavidxyz@gmail.com>
2023-06-22 11:24:57 +0200

    feat: petit vehicule electrique

--- src/components/livraison/data.js
+++ src/components/livraison/data.js
@@ -38,7 +38,7 @@ export const retraits = [
 
 export const relays = [
   { displayed: "En voiture", uid: "voiture_thermique", publicode: "voiture" },
-  { displayed: "En petit véhicule électrique", uid: "electrique", publicode: "petit véhicule électrique" },
+  { displayed: "En voiture électrique", uid: "electrique", publicode: "petit véhicule électrique" },
   { displayed: "Marche", uid: "marche", publicode: "marche" },
   { displayed: "En vélo", uid: "magasin", publicode: "vélo" },
   { displayed: "En transport en commun", uid: "transport", publicode: "commun" },
