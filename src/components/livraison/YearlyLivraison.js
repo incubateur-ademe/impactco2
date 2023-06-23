708c5b1945f99f0a9fda7b58b6352708da4c2dd7
David Boureau <bdavidxyz@gmail.com>
2023-06-23 10:45:41 +0200

    style: better yearly text

--- src/components/livraison/YearlyLivraison.js
+++ src/components/livraison/YearlyLivraison.js
@@ -4,23 +4,13 @@ import styled from "styled-components";
 export default function YearlyLivraison(props) {
   return (
     <Wrapper>
-      <br />
       <Text>
-        Vos usages émettent{" "}
-        <Big>
-          {convertGramsToKilograms(props.co2eq)} kg CO<sub>2</sub>e <Color>par livraison</Color>
-        </Big>{" "}
-        <Disclaimer>
-          Cette valeur est une estimation basée sur les paramètres fournis, ainsi que d'autres paramètres non
-          représentés ici, mais dont la valeur est calculée sur les moyennes nationales.
-        </Disclaimer>
-      </Text>
-      <Text>
-        Soit{" "}
-        <Big>
-          {convertGramsToKilograms(props.co2eq * 12)} kg CO<sub>2</sub>e <Color>par an</Color>{" "}
-        </Big>
-        <div>Cette valeur se base sur la fréquence d'une commande par mois</div>
+        Vos usages émettent donc{" "}
+        <Color>
+          {convertGramsToKilograms(props.co2eq)} kg CO<sub>2</sub>e
+        </Color>{" "}
+        <strong>par an</strong>
+        <span> (cette valeur se base sur la fréquence d'une commande par mois).</span>
       </Text>
       <br />
     </Wrapper>
@@ -30,27 +20,15 @@ export default function YearlyLivraison(props) {
 const Wrapper = styled.div``;
 
 const Text = styled.div`
-  font-size: 1.125rem;
+  font-size: 1rem;
   margin-top: 1rem;
   text-align: left;
   ${(props) => props.theme.mq.small} {
-    font-size: 0.875rem;
+    font-size: 0.4rem;
   }
 `;
-const Big = styled.span`
-  font-size: 1.375rem;
-  font-weight: bold;
 
-  ${(props) => props.theme.mq.small} {
-    display: block;
-    font-size: 1.25rem;
-  }
-`;
 const Color = styled.span`
-  color: ${(props) => props.theme.colors.main};
-`;
-const Disclaimer = styled.span`
-  display: block;
-  font-size: 0.75rem;
-  font-weight: 300;
+  color: #457be7;
+  font-weight: bold;
 `;
