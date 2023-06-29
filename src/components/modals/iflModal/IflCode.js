999ae1deef8125a35845b9d71353d346de995e34
David Boureau <bdavidxyz@gmail.com>
2023-06-29 17:03:15 +0200

    style: copy code correctly

--- src/components/modals/iflModal/IflCode.js
+++ src/components/modals/iflModal/IflCode.js
@@ -26,32 +26,32 @@ export default function IflCode(props) {
       </Label>
       <Text name="code" copied={copied}>
         {script}
+        <Flex>
+          <div>
+            <svg ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20" width="16px">
+              {" "}
+              <path
+                stroke="#457be7"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M2 5a1 1 0 0 0-1 1v12a.969.969 0 0 0 .933 1h8.1a1 1 0 0 0 1-1.033M10 1v4a1 1 0 0 1-1 1H5m10-4v12a.97.97 0 0 1-.933 1H5.933A.97.97 0 0 1 5 14V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 9.828 1h4.239A.97.97 0 0 1 15 2Z"
+              />{" "}
+            </svg>
+          </div>
+          <div>
+            <Copy
+              onClick={() => {
+                if (!copied && copy(script)) {
+                  setCopied(true);
+                }
+              }}
+            >
+              Copier le code
+            </Copy>
+          </div>
+        </Flex>
       </Text>
-      <Flex>
-        <div>
-          <svg ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20" width="16px">
-            {" "}
-            <path
-              stroke="#457be7"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M2 5a1 1 0 0 0-1 1v12a.969.969 0 0 0 .933 1h8.1a1 1 0 0 0 1-1.033M10 1v4a1 1 0 0 1-1 1H5m10-4v12a.97.97 0 0 1-.933 1H5.933A.97.97 0 0 1 5 14V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 9.828 1h4.239A.97.97 0 0 1 15 2Z"
-            />{" "}
-          </svg>
-        </div>
-        <div>
-          <Copy
-            onClick={() => {
-              if (!copied && copy(script)) {
-                setCopied(true);
-              }
-            }}
-          >
-            Copier le code
-          </Copy>
-        </div>
-      </Flex>
     </Wrapper>
   );
 }
@@ -80,6 +80,7 @@ const Text = styled.code`
   display: block;
   font-size: 0.875rem;
   padding: 1rem 1rem 2rem;
+  padding-bottom: 0.5rem;
   word-break: break-word;
 `;
 const Copy = styled.button`
@@ -95,5 +96,7 @@ const Copy = styled.button`
 const Flex = styled.div`
   align-items: center;
   display: flex;
+  font-family: "Marianne";
   justify-content: flex-end;
+  margin-top: 1.5rem;
 `;
