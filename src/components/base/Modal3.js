80a394d1d098f40ca3c451114bf1c39f21ad4df5
David Boureau <bdavidxyz@gmail.com>
2023-07-11 19:43:27 +0200

    feat: decent modal trap

--- src/components/base/Modal3.js
+++ src/components/base/Modal3.js
@@ -31,14 +31,7 @@ export default function Modal3(props) {
                 }}
               >
                 Fermer
-                <svg
-                  id="button-close-modal"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                 </svg>
               </ButtonClose>
@@ -107,7 +100,9 @@ const ButtonClose = styled.button`
     margin-left: 0.5rem;
   }
   &:focus {
-    color: blue;
+    border-radius: 2pt;
+    outline: solid;
+    outline-color: revert;
   }
 `;
 
