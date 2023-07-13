aef7e0665c15ddfdeea919236c2d68460a85b910
David Boureau <bdavidxyz@gmail.com>
2023-07-13 07:55:51 +0200

    refactor: closer factor

--- src/scripts/build_fruits_et_legumes.js
+++ src/scripts/build_fruits_et_legumes.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-// const fruitsetlegumes = require('src/data/categories/fruitsetlegumes.json');
 
 const fs = require("fs");
 let rawdata = fs.readFileSync("src/data/categories/fruitsetlegumes.json");
@@ -41,37 +40,37 @@ const adaptEcv = (remotes) => {
     let agriculture = localFruit.ecv.find((e) => e.id === AGRICULTURE_ID);
     agriculture.id = AGRICULTURE_ID;
     agriculture.name = "agriculture";
-    agriculture.value = remote["Score_unique_EF_-_Agriculture"] * 10;
+    agriculture.value = remote["Score_unique_EF_-_Agriculture"] * 7;
     upsert(localFruit.ecv, agriculture);
 
     let transformation = localFruit.ecv.find((e) => e.id === TRANSFORMATION_ID) || {};
     transformation.id = TRANSFORMATION_ID;
     transformation.name = "transformation";
-    transformation.value = remote["Score_unique_EF_-_Transformation"] * 10;
+    transformation.value = remote["Score_unique_EF_-_Transformation"] * 7;
     upsert(localFruit.ecv, transformation);
 
     let emballage = localFruit.ecv.find((e) => e.id === EMBALLAGE_ID) || {};
     emballage.id = EMBALLAGE_ID;
     emballage.name = "emballage";
-    emballage.value = remote["Score_unique_EF_-_Emballage"] * 10;
+    emballage.value = remote["Score_unique_EF_-_Emballage"] * 7;
     upsert(localFruit.ecv, emballage);
 
     let transport = localFruit.ecv.find((e) => e.id === TRANSPORT_ID) || {};
     transport.id = TRANSPORT_ID;
     transport.name = "transport";
-    transport.value = remote["Score_unique_EF_-_Transport"] * 10;
+    transport.value = remote["Score_unique_EF_-_Transport"] * 7;
     upsert(localFruit.ecv, transport);
 
     let supermarche = localFruit.ecv.find((e) => e.id === SUPERMARCHE_ID) || {};
     supermarche.id = SUPERMARCHE_ID;
     supermarche.name = "supermarche";
-    supermarche.value = remote["Score_unique_EF_-_Supermarché_et_distribution"] * 10;
+    supermarche.value = remote["Score_unique_EF_-_Supermarché_et_distribution"] * 7;
     upsert(localFruit.ecv, supermarche);
 
     let consommation = localFruit.ecv.find((e) => e.id === CONSOMMATION_ID) || {};
     consommation.id = CONSOMMATION_ID;
     consommation.name = "consommation";
-    consommation.value = remote["Score_unique_EF_-_Consommation"] * 10;
+    consommation.value = remote["Score_unique_EF_-_Consommation"] * 7;
     upsert(localFruit.ecv, supermarche);
 
     console.log("localFruit", localFruit);
