98f9205f79c819d1e4f477a30247b7e37c1195c2
David Boureau <bdavidxyz@gmail.com>
2023-07-17 13:54:12 +0200

    feat: wording

--- src/components/livraison/CalculateurLivraison.js
+++ src/components/livraison/CalculateurLivraison.js
@@ -86,7 +86,7 @@ export default function CalculateurLivraison() {
                   d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"
                 />
               </svg>
-              &nbsp;Partagez
+              &nbsp;Partager
             </ButtonChange>
             <ButtonChange onClick={() => setIfl(true)} className="noscreenshot">
               <svg width="16px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
