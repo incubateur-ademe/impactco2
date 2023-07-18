071ef880b501183893061c4165f99f86c1624dbc
David Boureau <bdavidxyz@gmail.com>
2023-07-18 15:17:12 +0200

    feat: better text positionning

--- src/components/livraison/ConclusionLivraison.js
+++ src/components/livraison/ConclusionLivraison.js
@@ -8,10 +8,16 @@ export default function ConclusionLivraison() {
       <Wrapper>
         <Section2>
           <Section2.InnerMargin>
-            <Icon>
-              <Emojis>📖</Emojis>
-            </Icon>
-            <SimpleText>Pour plus de conseils,</SimpleText>
+            <UpperBubble>
+              <Icon>
+                <Emojis>📖</Emojis>
+              </Icon>
+            </UpperBubble>
+            <TextContainer>
+              <SimpleText>
+                Pour plus de conseils, télécharger le guide de l’ADEME « E-consommateur & responsable »
+              </SimpleText>
+            </TextContainer>
           </Section2.InnerMargin>
         </Section2>
       </Wrapper>
@@ -21,15 +27,32 @@ export default function ConclusionLivraison() {
 
 const Wrapper = styled.div`
   background-color: #ebf2ff;
+  margin-bottom: 2rem;
+  margin-top: 2.5rem;
 `;
 
 const SimpleText = styled.div`
   color: ${(props) => props.theme.colors.deepDark};
 `;
 
+const UpperBubble = styled.div`
+  position: relative;
+`;
+
 const Icon = styled.div`
+  background: #ebf2ff;
+  border-radius: 16px;
+  position: absolute;
+  top: -21px;
   > span {
-    display: flex;
-    justify-content: flex-start;
+    display: block;
+    font-size: 2rem;
+    margin-bottom: 0;
+    padding: 0.3rem;
+    text-align: left;
   }
+`;
+
+const TextContainer = styled.div`
+  padding: 1.5rem 0 1.5rem 3rem;
 `;
