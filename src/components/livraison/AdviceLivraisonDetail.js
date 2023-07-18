35f56920c2651677cd161264f2a592ff1524d504
David Boureau <bdavidxyz@gmail.com>
2023-07-18 11:48:39 +0200

    style: new background

--- src/components/livraison/AdviceLivraisonDetail.js
+++ src/components/livraison/AdviceLivraisonDetail.js
@@ -4,7 +4,9 @@ import styled from "styled-components";
 export default function AdviceLivraisonDetail(props) {
   return (
     <>
-      <H3Title>{props.title}</H3Title>
+      <Wrapper>
+        <H3Title>{props.title}</H3Title>
+      </Wrapper>
     </>
   );
 }
@@ -14,4 +16,9 @@ const H3Title = styled.h3`
   font-size: 16px;
   font-weight: 700;
   line-height: 24px;
+`;
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.darkBackground};
+  border-radius: 8px;
 `;
