c03240a670f57bbafd3d4673883a0104ecf37219
David Boureau <bdavidxyz@gmail.com>
2023-07-18 15:00:49 +0200

    feat: added emoji inside conclusion

--- src/components/livraison/ConclusionLivraison.js
+++ src/components/livraison/ConclusionLivraison.js
@@ -1,9 +1,35 @@
+import Section2 from "components/base/Section2";
+import { Emojis } from "components/misc/Visualization";
 import styled from "styled-components";
 
 export default function ConclusionLivraison() {
-  return <Wrapper>Conclusion</Wrapper>;
+  return (
+    <>
+      <Wrapper>
+        <Section2>
+          <Section2.InnerMargin>
+            <Icon>
+              <Emojis>📖</Emojis>
+            </Icon>
+            <SimpleText>Pour plus de conseils,</SimpleText>
+          </Section2.InnerMargin>
+        </Section2>
+      </Wrapper>
+    </>
+  );
 }
 
 const Wrapper = styled.div`
-  background-color: blue;
+  background-color: #ebf2ff;
+`;
+
+const SimpleText = styled.div`
+  color: ${(props) => props.theme.colors.deepDark};
+`;
+
+const Icon = styled.div`
+  > span {
+    display: flex;
+    justify-content: flex-start;
+  }
 `;
