dff629749031d2cb96329236a384240875e0768a
David Boureau <bdavidxyz@gmail.com>
2023-08-10 16:11:53 +0200

    feat: new utility function

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -12,6 +12,26 @@ export function formatNumber(value, noformat) {
   tempTotal = tempTotal > 5 ? Math.round(tempTotal * 1) / 1 : tempTotal;
   return noformat ? tempTotal : tempTotal.toLocaleString("fr-fr", { maximumFractionDigits: 11 });
 }
+
+export function getNumberWithNDigitsAfterComma(number, n) {
+  // Check if the number is a valid number.
+  if (!Number.isFinite(number)) {
+    return NaN;
+  }
+
+  // Get the number of digits after the decimal point.
+  const decimalPointIndex = number.toString().indexOf(".");
+  const numberOfDigitsAfterComma = decimalPointIndex === -1 ? 0 : number.toString().length - decimalPointIndex - 1;
+
+  // If the number of digits after the decimal point is less than or equal to N, then simply return the number.
+  if (numberOfDigitsAfterComma <= n) {
+    return number;
+  }
+
+  // Otherwise, round the number to N digits after the decimal point and return it.
+  return Math.round(number * 10 ** n) / 10 ** n;
+}
+
 export function formatNumberPrecision(value) {
   const tempValue = value < 1 ? value * 1000 : value;
   return (
