44e1f4dbd8cd2b1a5916af4b2c1f4671279fccee
David Boureau <bdavidxyz@gmail.com>
2023-08-10 15:32:48 +0200

    feat: text

--- src/components/modals/WarningNegaoctet.js
+++ src/components/modals/WarningNegaoctet.js
@@ -1,12 +1,30 @@
-import Modal from "components/base/Modal";
+import Modal2 from "components/base/Modal2";
 import ModalContext from "components/providers/ModalProvider";
 import React, { useContext } from "react";
+import styled from "styled-components";
 
 export default function WarningNegaoctet() {
   const { warningNegaoctet: open, setWarningNegaoctet: setOpen } = useContext(ModalContext);
+
+  const getTitle = () => {
+    return <Title>Source des données</Title>;
+  };
+
   return (
-    <Modal open={open} setOpen={setOpen}>
-      <h2>Avertissement</h2>
-    </Modal>
+    <Modal2 open={open} setOpen={setOpen} getTitle={getTitle}>
+      <p>
+        ⚠️ Attention : aujourd’hui pour calculer l’impact carbone de Stocker un Go de données, nous ne prenons pas en
+        compte dans nos calculs l’impact carbone de la transmission ni celui du stockage effectué par l'appareil,
+        puisque nous estimions que ce dernier étant partagé par beaucoup de personnes, était amorti.
+      </p>
+      <p>
+        Ces calculs, expliquent la différence d’impact carbone que l’on a entre le site impactco2.fr et le données NO.{" "}
+      </p>
+      <p>Nous travaillons à affiner ce calcul et le mettre à jour.</p>
+    </Modal2>
   );
 }
+
+const Title = styled.h1`
+  margin: 0;
+`;
--- src/components/views/equivalent/Details.js
+++ src/components/views/equivalent/Details.js
@@ -41,8 +41,13 @@ const Disclaimer = styled.p`
 const StyledMagicLink = styled(MagicLink)`
   font-size: 0.875rem;
 `;
+
+const ButtonWarning = styled.button`
+  cursor: pointer;
+`;
+
 export default function Details(props) {
-  const { setCo2e } = useContext(ModalContext);
+  const { setCo2e, setWarningNegaoctet } = useContext(ModalContext);
   console.log(props);
   return (
     <>
@@ -67,9 +72,26 @@ export default function Details(props) {
           </Disclaimer>
           {props?.equivalent?.slug === "stockagedonnee" ? (
             <>
-              <ButtonLink onClick={() => setCo2e(true)}>
-                CO<sub>2</sub>e
-              </ButtonLink>
+              <ButtonWarning onClick={() => setWarningNegaoctet(true)}>
+                Source
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-box-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                  />
+                </svg>
+              </ButtonWarning>
             </>
           ) : (
             <>
