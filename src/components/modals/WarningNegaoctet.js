7371c63075dd47f1f0d39a6c1b8df7dd044d3563
David Boureau <bdavidxyz@gmail.com>
2023-08-10 15:40:45 +0200

    feat: last tiny details

--- src/components/modals/WarningNegaoctet.js
+++ src/components/modals/WarningNegaoctet.js
@@ -1,4 +1,5 @@
 import Modal2 from "components/base/Modal2";
+import OutboundLink from "components/base/OutboundLink";
 import ModalContext from "components/providers/ModalProvider";
 import React, { useContext } from "react";
 import styled from "styled-components";
@@ -13,12 +14,26 @@ export default function WarningNegaoctet() {
   return (
     <Modal2 open={open} setOpen={setOpen} getTitle={getTitle}>
       <p>
+        Les données et les calculs sont issues de&nbsp;
+        <OutboundLink
+          href="https://base-empreinte.ademe.fr/documentation/base-impact"
+          title="Base impact, étude réalisée par Negaoctet"
+        >
+          l'étude NégaOctet 2022
+        </OutboundLink>
+        .
+      </p>
+      <p>
         ⚠️ Attention : aujourd’hui pour calculer l’impact carbone de Stocker un Go de données, nous ne prenons pas en
         compte dans nos calculs l’impact carbone de la transmission ni celui du stockage effectué par l'appareil,
         puisque nous estimions que ce dernier étant partagé par beaucoup de personnes, était amorti.
       </p>
       <p>
-        Ces calculs, expliquent la différence d’impact carbone que l’on a entre le site impactco2.fr et le données NO.{" "}
+        Ces calculs, expliquent la différence d’impact carbone que l’on a entre le site&nbsp;
+        <OutboundLink href="https://impactco2.fr" title="site Internet impactCO2">
+          impactco2.fr
+        </OutboundLink>{" "}
+        et les données NO.{" "}
       </p>
       <p>Nous travaillons à affiner ce calcul et le mettre à jour.</p>
     </Modal2>
