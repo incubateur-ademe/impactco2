e60bce886e23f00d6547b4a870a1dd49d05accce
David Boureau <bdavidxyz@gmail.com>
2023-08-10 11:32:45 +0200

    feat: upsert

--- src/scripts/build_boissons.js
+++ src/scripts/build_boissons.js
@@ -6,10 +6,12 @@ let rawboissons = JSON.parse(rawdata);
 let boissons = rawboissons.filter((e) => !!e?.Code_CIQUAL);
 // console.log("boissons", boissons);
 
-// const AGRICULTURE_ID = 30;
+const AGRICULTURE_ID = 30;
+// const TRANSFORMATION_ID = 31;
 // const EMBALLAGE_ID = 32;
 // const TRANSPORT_ID = 33;
 // const SUPERMARCHE_ID = 34;
+// const CONSOMMATION_ID = 35;
 
 const ciquals = boissons.map((e) => e.Code_CIQUAL).join(",");
 // console.log("ciquals", ciquals);
@@ -28,6 +30,20 @@ axios.get(remote_url).then((res) => {
   return finalResult;
 });
 
+function sumValues(prefix = "", remote = {}, array = []) {
+  let res = 0;
+  array.forEach((acc) => {
+    res += remote[`${prefix}${acc}`];
+  });
+  return res;
+}
+
+function upsert(array, element) {
+  const i = array.findIndex((_element) => _element.id === element.id);
+  if (i > -1) array[i] = element;
+  else array.push(element);
+}
+
 const adaptEcv = (remotes) => {
   let newList = boissons.map((boisson) => {
     let remote = remotes.find((r) => r.Code_CIQUAL === boisson.Code_CIQUAL);
@@ -36,6 +52,31 @@ const adaptEcv = (remotes) => {
     }
     let localBoisson = JSON.parse(JSON.stringify(boisson));
     console.log(localBoisson);
+
+    const finalities = [
+      "Agriculture",
+      "Transformation",
+      "Emballage",
+      "Transport",
+      "Supermarché_et_distribution",
+      "Consommation",
+    ];
+    const finalC02 = sumValues("Changement_climatique_-_", remote, finalities);
+    console.log("finalC02", finalC02);
+    const finalEF = sumValues("Score_unique_EF_-_", remote, finalities);
+    console.log("finalEF", finalEF);
+    const delta = finalC02 / finalEF;
+    console.log("delta", delta);
+
+    let agriculture = localBoisson.ecv.find((e) => e.id === AGRICULTURE_ID) || {};
+    agriculture.id = AGRICULTURE_ID;
+    agriculture.name = "agriculture";
+    agriculture.value = remote["Score_unique_EF_-_Agriculture"] * delta;
+    console.log("agriculture", agriculture);
+    upsert(localBoisson.ecv, agriculture);
+    console.log("localBoisson.ecv", localBoisson.ecv);
+    console.log("====================================================================================");
+    console.log("\\");
   });
   return newList;
 };
