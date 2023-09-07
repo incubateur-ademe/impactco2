885445ff15745343329f55ac546b5fdca6d5d31e
David Boureau <bdavidxyz@gmail.com>
2023-09-07 15:29:33 +0200

    refactor: cleaned logs

--- src/components/base/DocumentationLivraison.js
+++ src/components/base/DocumentationLivraison.js
@@ -5,9 +5,7 @@ import { RulePage } from "publicodes-react";
 import React, { useContext } from "react";
 
 export default function DocumentationLivraison(props) {
-  console.log("props", props);
   const { engine } = useContext(RulesContextLivraison);
-  console.log("engine", engine);
 
   return (
     <RulePage
