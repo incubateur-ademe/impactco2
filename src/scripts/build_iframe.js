ec47eefac4785c6d0e267ccc9e1e86775b49888a
David Boureau <bdavidxyz@gmail.com>
2023-09-18 11:49:49 +0200

    feat: trying to read custom env var

--- src/scripts/build_iframe.js
+++ src/scripts/build_iframe.js
@@ -5,6 +5,6 @@ webpack(config, (err, stats) => {
   if (err || stats.hasErrors()) {
     console.log(err);
   }
-  console.log(`https://${process.env.NODE_VERSION}/iframes/livraison/simulation`);
+  console.log(`https://${process.env.ZZZ}/iframes/livraison/simulation`);
   console.log("Done.......");
 });
