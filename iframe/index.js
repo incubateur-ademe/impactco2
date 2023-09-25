33c333803780b4b0fef4791eb28821ade1fcdff5
David Boureau <bdavidxyz@gmail.com>
2023-09-25 17:58:20 +0200

    feat: back to simple...

--- iframe/index.js
+++ iframe/index.js
@@ -1,7 +1,5 @@
 import { iframeResize } from "iframe-resizer";
 
-const isFigaro = !!document.getElementById("mon-impact-transport");
-
 const script =
   document.getElementById("mon-impact-transport") ||
   document.getElementById("datagir-teletravail") ||
@@ -40,21 +38,5 @@ const link = document.createElement("div");
 link.innerHTML = `<a href="https://impactco2.fr" target="_blank">Découvrez l'empreinte carbone des objets et gestes de votre quotidien</a>`;
 link.style.cssText = `margin: 0.5rem auto 1rem;text-align: center`;
 
-if (!isFigaro) {
-  script.parentNode.insertBefore(iframe, script);
-  script.parentNode.insertBefore(link, script);
-} else {
-  let simpleButtonScreen = document.createElement("div");
-  simpleButtonScreen.innerHTML = `
-  <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
-  <a target="_blank" title="Impact carbone de vos déplacements (ouvre le site impactCO2 dans un nouvel onglet)" href="https://impactco2.fr/transport/itineraire" style="background-color: rgb(38, 130, 124); color:white; cursor:pointer; display:block; padding:1rem; border-radius:8px; text-decoration: none; text-align: center;">
-    Découvrez l'impact carbone de vos déplacements
-  </a>
-  </div>
-  `;
-  script.parentNode.insertBefore(simpleButtonScreen, script);
-}
-
-console.log("isFig", isFigaro);
-console.log("window.location.ancestorOrigins[0]", window.location.ancestorOrigins[0]);
-console.log("document.referrer", document.referrer);
+script.parentNode.insertBefore(iframe, script);
+script.parentNode.insertBefore(link, script);
