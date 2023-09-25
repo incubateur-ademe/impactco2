9169295584a9dc40a13f8b13862163d551c598f2
David Boureau <bdavidxyz@gmail.com>
2023-09-25 12:11:33 +0200

    feat: logging at the end

--- iframe/index.js
+++ iframe/index.js
@@ -40,10 +40,6 @@ const link = document.createElement("div");
 link.innerHTML = `<a href="https://impactco2.fr" target="_blank">Découvrez l'empreinte carbone des objets et gestes de votre quotidien</a>`;
 link.style.cssText = `margin: 0.5rem auto 1rem;text-align: center`;
 
-console.log("isFig", isFigaro);
-console.log("window.location.ancestorOrigins[0]", window.location.ancestorOrigins[0]);
-console.log("document.referrer", document.referrer);
-
 if (!isFigaro) {
   script.parentNode.insertBefore(iframe, script);
   script.parentNode.insertBefore(link, script);
@@ -51,10 +47,14 @@ if (!isFigaro) {
   let simpleButtonScreen = document.createElement("div");
   simpleButtonScreen.innerHTML = `
   <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
-    <a target="_blank" title="Impact du transport sur le climat (ouvre le site impactCO2 dans un nouvel onglet)" href="https://impactco2.fr/transport/itineraire" style="background-color: rgb(38, 130, 124); color:white; cursor:pointer; display:block; padding:1rem; border-radius:8px; text-decoration: none; text-align: center;">
-      Découvrir l'impact carbone du transport sur le climat
-    </a>
+  <a target="_blank" title="Impact du transport sur le climat (ouvre le site impactCO2 dans un nouvel onglet)" href="https://impactco2.fr/transport/itineraire" style="background-color: rgb(38, 130, 124); color:white; cursor:pointer; display:block; padding:1rem; border-radius:8px; text-decoration: none; text-align: center;">
+  Découvrir l'impact carbone du transport sur le climat
+  </a>
   </div>
   `;
   script.parentNode.insertBefore(simpleButtonScreen, script);
 }
+
+console.log("isFig", isFigaro);
+console.log("window.location.ancestorOrigins[0]", window.location.ancestorOrigins[0]);
+console.log("document.referrer", document.referrer);
