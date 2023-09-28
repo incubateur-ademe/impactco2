16d16a11e304348fdcad0f9389f3d1b7789d18d3
David Boureau <bdavidxyz@gmail.com>
2023-09-28 14:38:56 +0200

    feat: debugging headers only

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
-  let host = req.headers.host;
-  let params = req.headers["x-invoke-query"];
-  let q = JSON.parse(decodeURIComponent(params));
-  let queryString = new URLSearchParams(q).toString();
-  res.status(200).json({ h: host, q: q, d: queryString });
+  // let host = req.headers.host;
+  // let params = req.headers["x-invoke-query"];
+  // let q = JSON.parse(decodeURIComponent(params));
+  // let queryString = new URLSearchParams(q).toString();
+  // res.status(200).json({ h: host, q: q, d: queryString });
+  res.status(200).json({ h: req.headers });
 }
