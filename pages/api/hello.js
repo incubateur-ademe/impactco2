320e49b74fce70a3b5042a4b18ce10173d55c06e
David Boureau <bdavidxyz@gmail.com>
2023-09-28 14:45:12 +0200

    feat: read query v2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
+  let headers = req.headers;
+  let query = req.query;
   // let host = req.headers.host;
   // let params = req.headers["x-invoke-query"];
   // let q = JSON.parse(decodeURIComponent(params));
   // let queryString = new URLSearchParams(q).toString();
   // res.status(200).json({ h: host, q: q, d: queryString });
-  res.status(200).json({ h: req.headers });
+  res.status(200).json({ h: headers, q: query });
 }
