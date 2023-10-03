6fc355087c4533f36cb5d3d66a597cbadee0f891
David Boureau <bdavidxyz@gmail.com>
2023-10-03 15:26:38 +0200

    feat: by cat ok soft-breeze-3233

--- src/components/modals/AllSearchCategory.js
+++ src/components/modals/AllSearchCategory.js
@@ -1,12 +1,21 @@
 import EquivalentSquare from "./tilesModal/EquivalentSquare";
+import categories from "data/categories.json";
 import React from "react";
 import styled from "styled-components";
 
 export default function AllSearchCategory(props) {
+  const theCategory = categories.find((oneCat) => {
+    return oneCat.slug === props.cat;
+  });
+  console.log("theCategory", theCategory);
+  const items = props.items.filter((oneItem) => {
+    return oneItem.item.category === theCategory.id;
+  });
   return (
     <Wrapper>
+      {theCategory.name}
       <Equivalents>
-        {props.items.map(({ item }) => (
+        {items.map(({ item }) => (
           <EquivalentSquare key={item.slug} checked={false} equivalent={item} />
         ))}
       </Equivalents>
