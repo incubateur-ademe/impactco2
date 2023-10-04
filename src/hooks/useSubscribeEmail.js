305ac1365a64e19a899c5eff6e4fe6caa9aafb5c
David Boureau <bdavidxyz@gmail.com>
2023-10-04 20:43:41 +0200

    refactor: commenting last netlify func cold-river-7529

--- src/hooks/useSubscribeEmail.js
+++ src/hooks/useSubscribeEmail.js
@@ -1,8 +1,11 @@
-import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+import { useMutation } from "@tanstack/react-query";
+
+// import axios from 'axios'
 
 export default function useSubscribeEmail() {
-  return useMutation((email) =>
-    axios.get(`/.netlify/functions/subscribeEmail?email=${email}`)
-  )
+  return useMutation((email) => {
+    // return axios.get(`/.netlify/functions/subscribeEmail?email=${email}`)
+    console.log("Function is not more in use for email starting with", email[0]);
+    return;
+  });
 }
