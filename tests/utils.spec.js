8d4391fe2fae2ee80fc79f527fd3913314439406
David Boureau <bdavidxyz@gmail.com>
2023-10-10 16:04:37 +0200

    feat: better test building

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,17 +1,21 @@
 import { test, expect } from "@playwright/test";
 import { buildCurrentUrlFor } from "utils/urls";
 
-test.describe("describe title", () => {
-  test("buildCurrentUrlFor returns an URL with http (without s) for a local URL", () => {
+test.describe("buildCurrentUrlFor", () => {
+  test("returns an URL with http (without s) for a local URL", () => {
     let res = buildCurrentUrlFor("localhost:3000", "");
     expect(res).toEqual("http://localhost:3000");
   });
-  test("buildCurrentUrlFor returns an URL with http (with s) for a remote URL", () => {
+  test("returns an URL with http (with s) for a remote URL", () => {
     let res = buildCurrentUrlFor("impactco2.fr", "");
     expect(res).toEqual("https://impactco2.fr");
   });
-  test("buildCurrentUrlFor may add a path if needed", () => {
+  test("may add a path if needed", () => {
     let res = buildCurrentUrlFor("impactco2.fr", "/livraison");
     expect(res).toEqual("https://impactco2.fr/livraison");
+  });
+  test("may say that base URL is not defined", () => {
+    let res = buildCurrentUrlFor(undefined, "/livraison");
+    expect(res).toEqual("https://notdefined/livraison");
   });
 });
