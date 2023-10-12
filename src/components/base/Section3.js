431de852e620b3027d464cb5e5cd9bf393264123
David Boureau <bdavidxyz@gmail.com>
2023-10-12 15:55:34 +0200

    feat: rebuilding footer

---
+++ src/components/base/Section3.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Section3 = styled.div``;
+
+Section3.WideContent = styled.div`
+  ${(props) => props.theme.mq.medium} {
+    width: 100vw;
+  }
+`;
+Section3.InnerMargin = styled.div`
+  margin-left: ${(props) => (props.embedded ? 2 : 4)}rem;
+  margin-right: ${(props) => (props.embedded ? 2 : 4)}rem;
+  ${(props) => props.theme.mq.xlarge} {
+    margin-left: ${(props) => (props.embedded ? 1.75 : 2.5)}rem;
+    margin-right: ${(props) => (props.embedded ? 1.75 : 2.5)}rem;
+  }
+  ${(props) => props.theme.mq.large} {
+    margin-left: ${(props) => (props.embedded ? 1.5 : 2)}rem;
+    margin-right: ${(props) => (props.embedded ? 1.5 : 2)}rem;
+  }
+  ${(props) => props.theme.mq.medium} {
+    margin-left: ${(props) => (props.embedded ? 1 : 1.5)}rem;
+    margin-right: ${(props) => (props.embedded ? 1 : 1.5)}rem;
+  }
+  ${(props) => props.theme.mq.small} {
+    margin-left: ${(props) => (props.embedded ? 0.5 : 0.75)}rem;
+    margin-right: ${(props) => (props.embedded ? 0.5 : 0.75)}rem;
+  }
+`;
+
+export default Section3;
--- src/components/layout/web/FooterBlue.js
+++ src/components/layout/web/FooterBlue.js
@@ -1,14 +1,62 @@
 import Ademe from "components/base/Ademe";
 import Logo from "components/base/Logo";
 import Marianne from "components/base/Marianne";
+import Section3 from "components/base/Section3";
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
+export default function FooterBlue(props) {
+  return (
+    <Wrapper mt={props.mt}>
+      <Section3>
+        <Section3.WideContent>
+          <Section3.InnerMargin>
+            <Grid>
+              <GridItem>
+                <Logos>
+                  <Marianne />
+                  <Ademe />
+                  <Logo />
+                </Logos>
+              </GridItem>
+              <GridItem>
+                <FooterExplain>
+                  <strong>Impact CO2</strong>
+                  <p>Le site de ressources qui vulgarise et valorise les données environnementales de l'ADEME</p>
+                </FooterExplain>
+              </GridItem>
+              <GridItem>
+                <FooterLinks>
+                  <FooterLink pb={"0"}>
+                    <Link href="/plan-du-site">Plan du site</Link>
+                  </FooterLink>
+                  <FooterLink pb={"0"}>
+                    <Link href="/accessibilite">Accessibilité : non-conforme</Link>
+                  </FooterLink>
+                  <FooterLink pb={"0"}>
+                    <Link href="/mentions-legales">Mentions légales</Link>
+                  </FooterLink>
+                  <FooterLink>
+                    <Link href="/politique-de-confidentialite">Politique de confidentialité</Link>
+                  </FooterLink>
+                </FooterLinks>
+              </GridItem>
+              <GridItem>
+                <FooterLink>Version : {process.env.thebuildid}</FooterLink>
+              </GridItem>
+            </Grid>
+          </Section3.InnerMargin>
+        </Section3.WideContent>
+      </Section3>
+    </Wrapper>
+  );
+}
+
 const Logos = styled.div`
   align-items: center;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   margin: 0 auto;
   padding: 0 0.75rem;
   text-decoration: none;
@@ -30,27 +78,15 @@ const Wrapper = styled.div`
   margin-top: ${(props) => props.mt || "0"};
 `;
 
-export default function FooterBlue(props) {
-  return (
-    <Wrapper mt={props.mt}>
-      <Logos>
-        <Marianne />
-        <Ademe />
-        <Logo />
-      </Logos>
-      <FooterLink pb={"0"}>
-        <Link href="/plan-du-site">Plan du site</Link>
-      </FooterLink>
-      <FooterLink pb={"0"}>
-        <Link href="/accessibilite">Accessibilité : non-conforme</Link>
-      </FooterLink>
-      <FooterLink pb={"0"}>
-        <Link href="/mentions-legales">Mentions légales</Link>
-      </FooterLink>
-      <FooterLink>
-        <Link href="/politique-de-confidentialite">Politique de confidentialité</Link>
-      </FooterLink>
-      <FooterLink>Version : {process.env.thebuildid}</FooterLink>
-    </Wrapper>
-  );
-}
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const GridItem = styled.div``;
+
+const FooterExplain = styled.div``;
+
+const FooterLinks = styled.div`
+  display: flex;
+`;
