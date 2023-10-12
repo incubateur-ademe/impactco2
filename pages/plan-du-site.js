c126d58e26a8e797b3c2bc643fa6243e92e84e44
David Boureau <bdavidxyz@gmail.com>
2023-10-12 14:12:30 +0200

    feat: level1 titles, unstyled

--- pages/plan-du-site.js
+++ pages/plan-du-site.js
@@ -11,9 +11,61 @@ export default function PlanDuSite() {
         <Section2.InnerMargin>
           <FormatText>
             <h1>Plan du site</h1>
-            <Link href="/" title="Page d'accueil" className="level1">
-              Page d'accueil
-            </Link>
+            <Level1>
+              <Link href="/" title="Page d'accueil" className="level1">
+                Page d'accueil
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Calculateur carbone" className="level1">
+                Calculateur carbone
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Diffuser les ressources" className="level1">
+                Diffuser les ressources
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Intégrer les ressources" className="level1">
+                Intégrer les ressources
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Qui sommes-nous ?" className="level1">
+                Qui sommes-nous ?
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Statistiques" className="level1">
+                Statistiques
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Nouveautés" className="level1">
+                Nouveautés
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Accessibilité (non conforme)" className="level1">
+                Accessibilité (non conforme)
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Diffuser" className="level1">
+                Diffuser
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Mentions légales" className="level1">
+                Mentions légales
+              </Link>
+            </Level1>
+            <Level1>
+              <Link href="/" title="Gestion des cookies" className="level1">
+                Gestion des cookies
+              </Link>
+            </Level1>
           </FormatText>
         </Section2.InnerMargin>
       </Section2>
@@ -23,9 +75,14 @@ export default function PlanDuSite() {
 
 const FormatText = styled.div`
   h1 {
-    font-size: 3rem;
+    font-size: 2.25rem;
+    font-weight: 800;
+    line-height: 44px;
+    letter-spacing: -0.01em;
   }
   > h2 {
     margin-top: 3rem;
   }
 `;
+
+const Level1 = styled.div``;
