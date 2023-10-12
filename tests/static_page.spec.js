76a929272b2cf727e2f1fd5c046e1db335bad95e
David Boureau <bdavidxyz@gmail.com>
2023-10-12 15:30:30 +0200

    feat: added tests

---
+++ tests/static_page.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Affichage sans erreur des pages statiques", async ({ page }) => {
+  await test.step("On peut accèder au plan du site", async () => {
+    await page.goto("/plan-du-site");
+    await expect(page).toHaveTitle(/Plan du site | Impact CO2/);
+  });
+  await test.step("On peut accèder aux mentions légales", async () => {
+    await page.goto("/mentions-legales");
+    await expect(page).toHaveTitle(/Mentions légales | Impact CO2/);
+  });
+});
