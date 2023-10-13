f2188e924d71388c7313d389af972081ed6b23d6
David Boureau <bdavidxyz@gmail.com>
2023-10-13 18:32:36 +0200

    style: small diff on focus

--- src/components/misc/search/NavSearchBar.js
+++ src/components/misc/search/NavSearchBar.js
@@ -10,7 +10,8 @@ const Wrapper = styled.form`
   background-color: ${(props) => (props.focus ? props.theme.colors.background : "transparent")};
   border: 1px solid #eae5e8;
   border-radius: 0.625em;
-  box-shadow: 0px 4px 10px 0px rgba(0, 17, 51, 0.06);
+  box-shadow: ${(props) =>
+    props.focus ? "0px 4px 10px 0px rgba(0, 17, 51, 0.08)" : "0px 4px 10px 0px rgba(0, 17, 51, 0.04)"};
   left: 0;
   overflow: hidden;
   position: absolute;
