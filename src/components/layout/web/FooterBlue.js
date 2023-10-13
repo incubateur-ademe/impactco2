76f611d15aedc4f9cf06cebb6ebe3dbfdc69f3ea
David Boureau <bdavidxyz@gmail.com>
2023-10-13 10:00:21 +0200

    feat: responsive footer

--- src/components/layout/web/FooterBlue.js
+++ src/components/layout/web/FooterBlue.js
@@ -28,16 +28,24 @@ export default function FooterBlue(props) {
               </div>
               <div className="gridlinks">
                 <FooterLink>
-                  <Link href="/plan-du-site">Plan du site</Link>
+                  <Link href="/plan-du-site" title="Plan du site">
+                    Plan du site
+                  </Link>
                 </FooterLink>
                 <FooterLink>
-                  <Link href="/accessibilite">Accessibilité : non-conforme</Link>
+                  <Link href="/accessibilite" title="Accessibilité (non conforme)">
+                    Accessibilité
+                  </Link>
                 </FooterLink>
                 <FooterLink>
-                  <Link href="/mentions-legales">Mentions légales</Link>
+                  <Link href="/mentions-legales" title="Mentions légales">
+                    Mentions légales
+                  </Link>
                 </FooterLink>
                 <FooterLink>
-                  <Link href="/politique-de-confidentialite">Politique de confidentialité</Link>
+                  <Link href="/politique-de-confidentialite" title="Gestion des cookies">
+                    Gestion des cookies
+                  </Link>
                 </FooterLink>
                 <Version>Version : {process.env.thebuildid}</Version>
               </div>
@@ -53,7 +61,7 @@ const Logos = styled.div`
   align-items: center;
   display: flex;
   justify-content: flex-start;
-  margin: 0 auto;
+  margin: 3rem 0 3rem 0;
   padding: 0 0.75rem;
   text-decoration: none;
 
@@ -61,14 +69,28 @@ const Logos = styled.div`
     font-size: ${(props) => (props.iframe ? 0.75 : 1)}rem;
     padding: 0 0.25rem;
   }
+  ${(props) => props.theme.mq.medium} {
+    margin: 1.5rem 0 0 0;
+  }
 `;
+
+const FooterExplain = styled.div`
+  margin: 3rem 0 3rem 0;
+  p {
+    margin-bottom: 0;
+  }
+  ${(props) => props.theme.mq.medium} {
+    margin: 1.5rem 0 1.5rem 0;
+  }
+`;
+
 const FooterLink = styled.div`
   a {
-    background-color: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.deepDarkReversible};
     font-size: 0.75rem;
     font-weight: 400;
     letter-spacing: 0em;
-    margin-left: 0.5rem;
+    margin-left: 1.5rem;
     ${(props) => props.theme.mq.medium} {
       margin-left: inherit;
     }
@@ -98,13 +120,13 @@ const Grid = styled.div`
   }
 `;
 
-const FooterExplain = styled.div``;
-
 const Version = styled.div`
   font-size: 0.75rem;
   font-weight: 300;
   margin-left: auto;
+  margin-top: inherit;
   ${(props) => props.theme.mq.medium} {
     margin-left: inherit;
+    margin-top: 1rem;
   }
 `;
