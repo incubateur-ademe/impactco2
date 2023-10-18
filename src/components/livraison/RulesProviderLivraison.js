e3d72d94d781b999e0133bf119a8f40467236da9
Clement AUGER <clem.auger@hotmail.fr>
2023-10-18 11:25:44 +0200

    feat: add useMemo + unit errors silent

--- src/components/livraison/RulesProviderLivraison.js
+++ src/components/livraison/RulesProviderLivraison.js
@@ -1,14 +1,15 @@
 import rules from "@incubateur-ademe/publicodes-impact-livraison";
 import useSituation from "hooks/useSituation";
 import Engine from "publicodes";
-import React from "react";
+import React, { useMemo } from "react";
 
 const RulesContextLivraison = React.createContext({});
 
 export function RulesProviderLivraison(props) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
 
-  const engine = new Engine(rules);
+  // @Clemog: We make unit errors silent.
+  const engine = useMemo(() => new Engine(rules, { logger: { log: () => {}, warn: () => {}, err: () => {} } }), []);
 
   const { situation, setSituation } = useSituation(engine);
 
--- src/components/numerique/RulesProvider.js
+++ src/components/numerique/RulesProvider.js
@@ -1,14 +1,15 @@
 import rules from "@incubateur-ademe/publicodes-negaoctet";
 import useSituation from "hooks/useSituation";
 import Engine from "publicodes";
-import React from "react";
+import React, { useMemo } from "react";
 
 const RulesContext = React.createContext({});
 
 export function RulesProvider(props) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
 
-  const engine = new Engine(rules);
+  // @Clemog: We make unit errors silent.
+  const engine = useMemo(() => new Engine(rules, { logger: { log: () => {}, warn: () => {}, err: () => {} } }), []);
 
   const { situation, setSituation } = useSituation(engine);
 
