9009b3e1392256cb35816107da92155037fb9af0
David Boureau <bdavidxyz@gmail.com>
2023-10-26 10:14:57 +0200

    added mock for localstorage

--- testc/EqModal4.test.js
+++ testc/EqModal4.test.js
@@ -1,16 +1,28 @@
+import { localStorageImpl } from "../test-utils/mock-local-storage";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import EqModal4 from "components/modals/EqModal4";
-import { StyleProvider } from "components/providers/StyleProvider";
+import { DataProvider } from "components/providers/DataProvider";
+import { ModalProvider } from "components/providers/ModalProvider";
+import { ThemeProvider } from "styled-components";
+import { themes } from "utils/styles";
 
 describe("EqModal4", () => {
   it("renders a Modal to change equivalences", () => {
+    const { methods } = localStorageImpl.register();
     render(
-      <StyleProvider>
-        <EqModal4 />
-      </StyleProvider>
+      <DataProvider>
+        <ModalProvider>
+          <ThemeProvider theme={themes["default"]}>
+            <EqModal4 />
+          </ThemeProvider>
+        </ModalProvider>
+      </DataProvider>
     );
     // check if all components are rendered
-    expect(screen.getByTestId("magic-link")).toHaveAttribute("href", "https://notdefined");
+    expect(screen.getByTestId("eqs_selected")).toHaveText("blabla");
+    expect(methods.setItem).toBeCalledTimes(1);
+
+    localStorageImpl.unregister();
   });
 });
