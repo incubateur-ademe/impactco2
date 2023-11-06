9f981772ca2d1474a1735d694fa26d223212b39c
David Boureau <bdavidxyz@gmail.com>
2023-11-06 12:17:11 +0100

    refactor: working test

--- teste/partage_livraison.spec.js
+++ teste/partage_livraison.spec.js
@@ -12,11 +12,11 @@ test.beforeEach(async ({ page }) => {
 test("Le partage dirige bien vers la bonne URL", async ({ page }) => {
   await test.step("On clique sur le bouton de partage en haut, une URL correcte est proposée", async () => {
     await page.locator("#shareUp").click();
-    await expect(page.locator("#shareUrl")).toHaveValue("http://localhost:3000/livraison");
+    await expect(page.locator("#shareUrl")).toHaveValue("https://notdefined/livraison");
   });
   await test.step("On clique sur le bouton de partage en bas, une URL correcte est proposée", async () => {
     await page.locator("#button-close").click();
     await page.locator("#shareDown").click();
-    await expect(page.locator("#shareUrl")).toHaveValue("http://localhost:3000/livraison#ressource");
+    await expect(page.locator("#shareUrl")).toHaveValue("https://notdefined/livraison#ressource");
   });
 });
