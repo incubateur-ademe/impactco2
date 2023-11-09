0505871deff3619085b76e016c0016278134db4e
David Boureau <bdavidxyz@gmail.com>
2023-11-09 16:13:02 +0100

    feat: all tests pass

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,8 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/', '<rootDir>/src/'],
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/testc/**/*.test.js', '**/testu/**/*.test.js', '**/testu/**/*.test.ts'],
+  moduleNameMapper: {
+    '@incubateur-ademe/publicodes-impact-livraison': 'test-mock/livraison.json',
+  },
 }
 module.exports = createJestConfig(customJestConfig)
