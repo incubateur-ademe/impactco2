0c60ed0acdceafe1d7ea02286c0315b54249d2b9
David Boureau <bdavidxyz@gmail.com>
2023-11-10 15:14:57 +0100

    refactor: playwright conf without require

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,5 @@
 // @ts-check
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { defineConfig, devices } = require('@playwright/test')
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -11,7 +10,7 @@ const { defineConfig, devices } = require('@playwright/test')
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-module.exports = defineConfig({
+export default defineConfig({
   expect: {
     timeout: 10 * 1000,
   },
