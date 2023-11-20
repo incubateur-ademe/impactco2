9fb418db6b608a56a05d2dd5254291fd3d8c5f28
David Boureau <bdavidxyz@gmail.com>
2023-11-20 19:06:23 +0100

    feat: 7 valid tests

--- testa/notion.test.js
+++ testa/notion.test.js
@@ -1,7 +1,16 @@
+import axios from 'axios'
 import { createMocks } from 'node-mocks-http'
 import notion from 'pages/api/notion'
+import { trackAPIRequest } from 'utils/middleware'
 
+jest.mock('utils/middleware', () => ({
+  trackAPIRequest: jest.fn(),
+}))
+jest.mock('axios')
 describe('/api/notion', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
   test('retourne une 405 si pas de POST', async () => {
     // Create mock request and response objects
     const { req, res } = createMocks({
@@ -15,7 +24,7 @@ describe('/api/notion', () => {
     // Assert the expected behavior
     expect(res._getStatusCode()).toBe(405)
   })
-  test('retourne une 400 si les entrées sont non valides', async () => {
+  test("retourne une 400 si pas d'entrée", async () => {
     // Create mock request and response objects
     const { req, res } = createMocks({
       method: 'POST',
@@ -27,5 +36,90 @@ describe('/api/notion', () => {
 
     // Assert the expected behavior
     expect(res._getStatusCode()).toBe(400)
+  })
+  test('retourne une 400 si les entrées sont non valides', async () => {
+    // Create mock request and response objects
+    const { req, res } = createMocks({
+      method: 'POST',
+      url: '/api/notion',
+      body: {
+        type: 'foo',
+        email: 'bar',
+      },
+    })
+
+    // Call the route function with the mock objects
+    await notion(req, res)
+
+    // Assert the expected behavior
+    expect(res._getStatusCode()).toBe(400)
+  })
+  test('retourne une 201 si les entrées sont valides', async () => {
+    // Create mock request and response objects
+    const { req, res } = createMocks({
+      method: 'POST',
+      url: '/api/notion',
+      body: {
+        type: 'contact',
+        email: 'valid@email.com',
+      },
+    })
+
+    // Call the route function with the mock objects
+    await notion(req, res)
+
+    // Assert the expected behavior
+    expect(res._getStatusCode()).toBe(201)
+  })
+  test("Demande à l'API d'être tracée si les entrées sont valides", async () => {
+    // Create mock request and response objects
+    const { req, res } = createMocks({
+      method: 'POST',
+      url: '/api/notion',
+      body: {
+        type: 'contact',
+        email: 'valid@email.com',
+      },
+    })
+
+    // Call the route function with the mock objects
+    await notion(req, res)
+
+    // Assert the expected behavior
+    expect(trackAPIRequest).toHaveBeenCalled()
+  })
+  test("Ne Demande pas à l'API d'être tracée si les entrées sont invalides", async () => {
+    // Create mock request and response objects
+    const { req, res } = createMocks({
+      method: 'POST',
+      url: '/api/notion',
+      body: {
+        type: 'foo',
+        email: 'bar',
+      },
+    })
+
+    // Call the route function with the mock objects
+    await notion(req, res)
+
+    // Assert the expected behavior
+    expect(trackAPIRequest).not.toHaveBeenCalled()
+  })
+  test("N'appelle pas notion si les entrées sont invalides", async () => {
+    // Create mock request and response objects
+    const { req, res } = createMocks({
+      method: 'POST',
+      url: '/api/notion',
+      body: {
+        type: 'foo',
+        email: 'bar',
+      },
+    })
+
+    // Call the route function with the mock objects
+    await notion(req, res)
+
+    // Assert the expected behavior
+    expect(axios).not.toHaveBeenCalled()
   })
 })
