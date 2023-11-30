3584300ae793acabf2fc76964d8295294a2ab37b
David Boureau <bdavidxyz@gmail.com>
2023-11-30 16:20:39 +0100

    feat: wiup

--- teste/usage_numerique.spec.js
+++ teste/usage_numerique.spec.js
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 import pti from 'playwright-to-istanbul'
-import v8toIstanbul from 'v8-to-istanbul'
+
+// import v8toIstanbul from 'v8-to-istanbul'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/usagenumerique')
@@ -14,11 +15,11 @@ test("Par défaut, des valeurs s'affichent pour les usages", async ({ page }) =>
   await page.coverage.startJSCoverage()
   await expect(page.getByTestId('impactNumeriqueTotal')).toHaveText('0,3 kg CO2e par semaine')
   const coverage = await page.coverage.stopJSCoverage()
-  for (const entry of coverage) {
-    const converter = v8toIstanbul('', 0, { source: entry.source })
-    await converter.load()
-    converter.applyCoverage(entry.functions)
-    pti.write([...coverage], { includeHostname: true, storagePath: './.nyc_output' })
-    // console.log(JSON.stringify(converter.toIstanbul()))
-  }
+  pti.write([...coverage], { includeHostname: true, storagePath: './.nyc_output' })
+  // for (const entry of coverage) {
+  //   const converter = v8toIstanbul('', 0, { source: entry.source })
+  //   await converter.load()
+  //   converter.applyCoverage(entry.functions)
+  //   // console.log(JSON.stringify(converter.toIstanbul()))
+  // }
 })
