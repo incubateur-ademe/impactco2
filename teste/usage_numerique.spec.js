5f1838edc1c0cdfb6cb5c43701c907c2c1a0c92c
David Boureau <bdavidxyz@gmail.com>
2023-11-30 16:56:59 +0100

    feat: removed unused

--- teste/usage_numerique.spec.js
+++ teste/usage_numerique.spec.js
@@ -1,10 +1,30 @@
 import { expect, test } from '@playwright/test'
-import pti from 'playwright-to-istanbul'
-
-// import v8toIstanbul from 'v8-to-istanbul'
+import fs from 'fs'
+import v8toIstanbul from 'v8-to-istanbul'
 
 test.beforeEach(async ({ page }) => {
+  await page.coverage.startJSCoverage()
   await page.goto('/usagenumerique')
+})
+test.afterEach(async ({ page }, testInfo) => {
+  const coverage = await page.coverage.stopJSCoverage()
+  let finalCovObj = {}
+  for (const entry of coverage) {
+    const converter = v8toIstanbul('', 0, { source: entry.source })
+    await converter.load()
+    converter.applyCoverage(entry.functions)
+    const jsonCovered = converter.toIstanbul()
+    const validObject = getValidObject(Object.values(jsonCovered)[0])
+    if (validObject) {
+      finalCovObj = { ...finalCovObj, ...validObject }
+    }
+  }
+  if (!fs.existsSync('coverage')) {
+    fs.mkdirSync('coverage')
+  }
+  fs.writeFileSync(`coverage/coverage-${slugify(testInfo.title)}.json`, JSON.stringify(finalCovObj, null, 1))
+  // pti.write([...coverage], { includeHostname: true, storagePath: './.nyc_output' })
+  // console.log('testInfo: ', testInfo)
 })
 
 test('La page des usages numériques se charge correctement', async ({ page }) => {
@@ -12,10 +32,7 @@ test('La page des usages numériques se charge correctement', async ({ page }) =
   await expect(page.getByRole('heading').first()).toHaveText("Découvrez l'impact des usages numériquessur le climat")
 })
 test("Par défaut, des valeurs s'affichent pour les usages", async ({ page }) => {
-  await page.coverage.startJSCoverage()
   await expect(page.getByTestId('impactNumeriqueTotal')).toHaveText('0,3 kg CO2e par semaine')
-  const coverage = await page.coverage.stopJSCoverage()
-  pti.write([...coverage], { includeHostname: true, storagePath: './.nyc_output' })
   // for (const entry of coverage) {
   //   const converter = v8toIstanbul('', 0, { source: entry.source })
   //   await converter.load()
@@ -23,3 +40,24 @@ test("Par défaut, des valeurs s'affichent pour les usages", async ({ page }) =>
   //   // console.log(JSON.stringify(converter.toIstanbul()))
   // }
 })
+
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+
+function getValidObject(obj) {
+  let localObj = JSON.parse(JSON.stringify(obj))
+  if (localObj && localObj.path) {
+    localObj.path = localObj.path.replaceAll('_N_E/', '')
+    localObj.path = localObj.path.split('?')[0]
+    if (localObj.path.indexOf('impactco2/src') > 0) {
+      console.log('ok for ' + localObj.path)
+    }
+    return localObj
+  }
+  return null
+}
