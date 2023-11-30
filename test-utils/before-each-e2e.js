dfbe5fc4e93bd5c9695c00873e031abff506163b
David Boureau <bdavidxyz@gmail.com>
2023-11-30 21:23:55 +0100

    feat: working version of factoring

---
+++ test-utils/before-each-e2e.js
@@ -0,0 +1,50 @@
+import fs from 'fs'
+import v8toIstanbul from 'v8-to-istanbul'
+
+export default function configurePlaywrightCoverage(test) {
+  test.beforeEach(async ({ page }) => {
+    await page.coverage.startJSCoverage()
+    await page.goto('/usagenumerique')
+  })
+  test.afterEach(async ({ page }, testInfo) => {
+    const coverage = await page.coverage.stopJSCoverage()
+    let finalCovObj = {}
+    for (const entry of coverage) {
+      const converter = v8toIstanbul('', 0, { source: entry.source })
+      await converter.load()
+      converter.applyCoverage(entry.functions)
+      const jsonCovered = converter.toIstanbul()
+      const validObject = getValidObject(Object.values(jsonCovered)[0])
+      if (validObject) {
+        finalCovObj = { ...finalCovObj, ...validObject }
+      }
+    }
+    if (!fs.existsSync('coverage')) {
+      fs.mkdirSync('coverage')
+    }
+    fs.writeFileSync(`coverage/coverage-${slugify(testInfo.title)}.json`, JSON.stringify(finalCovObj, null, 1))
+  })
+}
+
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+
+function getValidObject(obj) {
+  let res = null
+  let localObj = JSON.parse(JSON.stringify(obj))
+  if (localObj && localObj.path) {
+    localObj.path = localObj.path.replaceAll('_N_E/', '')
+    localObj.path = localObj.path.split('?')[0]
+    if (localObj.path.indexOf('impactco2/src') > 0 || localObj.path.indexOf('impactco2/pages') > 0) {
+      console.log('ok for ' + localObj.path)
+      res = {}
+      res[localObj.path] = localObj
+    }
+  }
+  return res
+}
--- teste/usage_numerique.spec.js
+++ teste/usage_numerique.spec.js
@@ -1,31 +1,7 @@
 import { expect, test } from '@playwright/test'
-import fs from 'fs'
-import v8toIstanbul from 'v8-to-istanbul'
+import configurePlaywrightCoverage from 'test-utils/before-each-e2e'
 
-test.beforeEach(async ({ page }) => {
-  await page.coverage.startJSCoverage()
-  await page.goto('/usagenumerique')
-})
-test.afterEach(async ({ page }, testInfo) => {
-  const coverage = await page.coverage.stopJSCoverage()
-  let finalCovObj = {}
-  for (const entry of coverage) {
-    const converter = v8toIstanbul('', 0, { source: entry.source })
-    await converter.load()
-    converter.applyCoverage(entry.functions)
-    const jsonCovered = converter.toIstanbul()
-    const validObject = getValidObject(Object.values(jsonCovered)[0])
-    if (validObject) {
-      finalCovObj = { ...finalCovObj, ...validObject }
-    }
-  }
-  if (!fs.existsSync('coverage')) {
-    fs.mkdirSync('coverage')
-  }
-  fs.writeFileSync(`coverage/coverage-${slugify(testInfo.title)}.json`, JSON.stringify(finalCovObj, null, 1))
-  // pti.write([...coverage], { includeHostname: true, storagePath: './.nyc_output' })
-  // console.log('testInfo: ', testInfo)
-})
+configurePlaywrightCoverage(test)
 
 test('La page des usages numériques se charge correctement', async ({ page }) => {
   await expect(page).toHaveTitle(/Impact Carbone de la livraison de colis | Impact CO2/)
@@ -40,26 +16,3 @@ test("Par défaut, des valeurs s'affichent pour les usages", async ({ page }) =>
   //   // console.log(JSON.stringify(converter.toIstanbul()))
   // }
 })
-
-const slugify = (str) =>
-  str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, '')
-    .replace(/[\s_-]+/g, '-')
-    .replace(/^-+|-+$/g, '')
-
-function getValidObject(obj) {
-  let res = null
-  let localObj = JSON.parse(JSON.stringify(obj))
-  if (localObj && localObj.path) {
-    localObj.path = localObj.path.replaceAll('_N_E/', '')
-    localObj.path = localObj.path.split('?')[0]
-    if (localObj.path.indexOf('impactco2/src') > 0 || localObj.path.indexOf('impactco2/pages') > 0) {
-      console.log('ok for ' + localObj.path)
-      res = {}
-      res[localObj.path] = localObj
-    }
-  }
-  return res
-}
