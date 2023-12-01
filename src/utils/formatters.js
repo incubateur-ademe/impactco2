769ce644f1aab7040ba0c7ea56c9dbb89bc7fdcd
David Boureau <bdavidxyz@gmail.com>
2023-12-01 15:14:50 +0100

    feat: formatConstruction in its own file

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,11 +2,6 @@ import { computeECVWithMultiplier } from './computeECV'
 import formatName from './formatName'
 import formatNumber from './formatNumber'
 
-export function formatConstruction(equivalent) {
-  return equivalent.total || equivalent.total === 0
-    ? equivalent.total
-    : equivalent.ecv.reduce((acc, cur) => acc + ([1, 2, 3, 4, 5].includes(cur.id) ? cur.value : 0), 0)
-}
 export function formatUsage(equivalent, years) {
   if (equivalent.usage) {
     return (years || equivalent.usage.defaultyears) * equivalent.usage.peryear
