3798290e3aafa4eb6a2e32c74c088c630f0b1a80
David Boureau <bdavidxyz@gmail.com>
2023-12-01 17:07:49 +0100

    feat: tested fullSentenceFormat

--- src/utils/fullSentenceFormat.js
+++ src/utils/fullSentenceFormat.js
@@ -2,7 +2,7 @@ import { computeECVWithMultiplier } from './computeECV'
 import formatName from './formatName'
 import formatNumber from './formatNumber'
 
-export function fullSentenceFormat(obj) {
+export default function fullSentenceFormat(obj) {
   return (
     formatNumber(obj.weight / computeECVWithMultiplier(obj.equivalent)) +
     ' ' +
