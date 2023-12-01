bbfffa8ad046e7fb01a6da9cb5270a532448a4e0
David Boureau <bdavidxyz@gmail.com>
2023-12-01 15:34:16 +0100

    feat: unique formatUsage

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,17 +2,6 @@ import { computeECVWithMultiplier } from './computeECV'
 import formatName from './formatName'
 import formatNumber from './formatNumber'
 
-export function formatUsage(equivalent, years) {
-  if (equivalent.usage) {
-    return (years || equivalent.usage.defaultyears) * equivalent.usage.peryear
-  }
-  if (equivalent?.ecv?.find((ecv) => [6, 7, 8].includes(ecv.id))) {
-    const usage = equivalent.ecv.reduce((acc, cur) => acc + ([6, 7, 8].includes(cur.id) ? cur.value : 0), 0)
-    return usage
-  }
-  return 0
-}
-
 export function fullSentenceFormat(obj) {
   return (
     formatNumber(obj.weight / computeECVWithMultiplier(obj.equivalent)) +
