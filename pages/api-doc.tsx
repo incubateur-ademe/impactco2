13111ea6bfc2ec7133e7e26e5a3caf9b78fa38e0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-12-04 19:46:50 +0100

    with ui

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,10 +1,21 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import React from 'react'
 import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
+import { Section, SectionWideContent } from 'components/base/Section'
+import Web from 'components/layout/Web'
 import { doc } from './api/v1/doc'
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />
+  return (
+    <Web>
+      <Section>
+        <SectionWideContent>
+          <SwaggerUI spec={spec} />
+        </SectionWideContent>
+      </Section>
+    </Web>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
