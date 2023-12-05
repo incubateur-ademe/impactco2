a9b5c1004831b85711617fed473402cf3ded0481
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-12-05 16:37:52 +0100

    feat: Add carpool to itinary

--- src/components/transport/Distance.js
+++ src/components/transport/Distance.js
@@ -1,20 +1,14 @@
-import React, { useContext } from 'react'
-import formatName from 'utils/formatName'
+import React from 'react'
 import useTransportations from 'hooks/useTransportations'
-import Checkbox from 'components/base/Checkbox'
 import { Section, SectionWideContent } from 'components/base/Section'
 import BarChart from 'components/charts/BarChart'
 import Bottom from 'components/misc/category/Bottom'
 import CategoryLegend from 'components/misc/category/CategoryLegend'
-import Instruction from 'components/misc/category/Instruction'
-import { Checkboxes, Top } from 'components/misc/category/Top'
 import Wrapper from 'components/misc/category/Wrapper'
+import ResultHeader from './ResultHeader'
 import Search from './Search'
-import TransportContext from './TransportProvider'
 
 export default function Distance(props) {
-  const { displayAll, setDisplayAll, carpool, setCarpool } = useContext(TransportContext)
-
   const transportations = useTransportations()
 
   return (
@@ -22,44 +16,10 @@ export default function Distance(props) {
       <SectionWideContent $small>
         <Wrapper name={props.category.title || props.category.name} slug={props.category.slug}>
           <Search distance iframe={props.iframe} />
-          {transportations.length ? (
-            <Top className='noscreenshot'>
-              <Instruction title={props.category.equivalent} gender={props.category.gender} />
-              <Checkboxes $visible>
-                <Checkbox
-                  name='displayAll'
-                  checked={displayAll}
-                  onChange={() => {
-                    setDisplayAll((prevDisplayAll) => !prevDisplayAll)
-                    window?.please?.track([
-                      'trackEvent',
-                      'Interaction',
-                      'Voir tous les équivalents',
-                      props.category.name,
-                    ])
-                  }}>
-                  Voir {props.category.gender === 'f' ? 'toutes' : 'tous'} les{' '}
-                  {formatName(props.category.equivalent, 2) || 'équivalents'}
-                </Checkbox>
-                <Checkbox
-                  name='carpool'
-                  checked={carpool}
-                  onChange={() => {
-                    setCarpool((prevCarpool) => (prevCarpool ? 0 : 2))
-                    window?.please?.track(['trackEvent', 'Interaction', 'Covoiturage'])
-                  }}>
-                  Afficher le covoiturage
-                </Checkbox>
-              </Checkboxes>
-            </Top>
-          ) : null}
+          <ResultHeader category={props.category} />
           <BarChart items={transportations} max={transportations[transportations.length - 1]?.value} />
-          {transportations.length && (
-            <>
-              <CategoryLegend />
-              <Bottom category={props.category} iframe={props.iframe} />
-            </>
-          )}
+          <CategoryLegend />
+          <Bottom category={props.category} iframe={props.iframe} />
         </Wrapper>
       </SectionWideContent>
     </Section>
