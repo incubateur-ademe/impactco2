6be1483136ea4eb53b4de4d4431a3b6e727de0bb
David Boureau <bdavidxyz@gmail.com>
2023-12-07 16:37:38 +0100

    feat: arrivee ok

--- teste/page_itinerary.spec.js
+++ teste/page_itinerary.spec.js
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
   await mockRoutes(page)
 })
 
-test('Recherche de la ville de départ', async ({ page }) => {
+test("Recherche de la ville de départ et d'arrivée", async ({ page }) => {
   await test.step('On charge la page itinéraire dans le navigateur', async () => {
     await page.goto('/transport/itineraire')
   })
@@ -34,6 +34,27 @@ test('Recherche de la ville de départ', async ({ page }) => {
   await test.step('Il y a bien des suggestions qui apparaissent', async () => {
     const suggestions = await getNbOfSuggestions(page)
     expect(suggestions).toEqual(7)
+  })
+
+  await test.step('On peut rentrer un des choix', async () => {
+    const nantes = await page.getByTestId('transportSuggest').locator('div').filter({ hasText: 'Nantes 44000 France' })
+    await nantes.click()
+    expect(page.getByTestId('Address-Départ').locator('form')).toHaveAttribute('addressset', 'Nantes  France')
+  })
+
+  await test.step('Arrivée - on peut rentrer directement 3 lettres, il y a aussi des suggestions qui apparaissent', async () => {
+    await page.getByPlaceholder('Arrivée').click({ force: true })
+    await page.keyboard.type('a')
+    await page.keyboard.type('n')
+    await page.keyboard.type('g')
+    const suggestions = await getNbOfSuggestions(page)
+    expect(suggestions).toEqual(7)
+  })
+
+  await test.step('Arrivée - On peut rentrer un des choix', async () => {
+    const angers = await page.getByTestId('transportSuggest').locator('div').filter({ hasText: 'Angers 49000 France' })
+    await angers.click()
+    expect(page.getByTestId('Address-Arrivée').locator('form')).toHaveAttribute('addressset', 'Angers  France')
   })
 })
 
