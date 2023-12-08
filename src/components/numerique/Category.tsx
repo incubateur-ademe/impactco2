73bab79816eee24cfb0e3f7735d34e769ebb691b
David Boureau <bdavidxyz@gmail.com>
2023-12-08 15:24:56 +0100

    feat: send emails works

--- src/components/numerique/Category.tsx
+++ src/components/numerique/Category.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
 import { Category as CategoryType } from 'types/category'
+import useLocalStorage from 'use-local-storage'
 import { Section, SectionWideContent } from 'components/base/Section'
 import Description from 'components/misc/category/Description'
 import Wrapper from 'components/misc/category/Wrapper'
@@ -8,7 +8,7 @@ import Result from './category/Result'
 import Search from './category/Search'
 
 export default function Category({ category }: { category: CategoryType }) {
-  const [numberEmails, setNumberEmails] = useState(50)
+  const [numberEmails, setNumberEmails] = useLocalStorage('numberEmails', 50)
 
   return (
     <Section $withoutPadding>
