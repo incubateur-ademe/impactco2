3d69be61f7303ba1c8843402e7b589635f238e16
David Boureau <bdavidxyz@gmail.com>
2023-12-11 14:55:45 +0100

    feat: fixing last failing tests

--- testc/TransportPage.test.js
+++ testc/TransportPage.test.js
@@ -21,7 +21,7 @@ describe('TransportPage - affiche la page itinéraire', () => {
     // When
     renderWithWrapper(<TransportPage category={getTransportCategory()} />)
     // Then
-    expect(await screen.findByTestId('velo')).toHaveTextContent('Vélo ou marche00 kg CO2e')
+    expect(await screen.findByTestId('bar-chart-item-velo')).toHaveTextContent('Vélo ou marche00 kg CO2e')
   })
   test("Par défaut, limite le nombre d'éléments affichés", async () => {
     // Given
@@ -29,7 +29,7 @@ describe('TransportPage - affiche la page itinéraire', () => {
     // When
     renderWithWrapper(<TransportPage category={getTransportCategory()} />)
     // Then
-    const velo = await screen.findByTestId('velo')
+    const velo = await screen.findByTestId('bar-chart-item-velo')
     const co2list = velo.parentElement.querySelectorAll('a')
     expect(co2list.length).toBe(7)
   })
@@ -37,7 +37,7 @@ describe('TransportPage - affiche la page itinéraire', () => {
     // Given
     mockRouter.push('/transport')
     renderWithWrapper(<TransportPage category={getTransportCategory()} />)
-    const velo = await screen.findByTestId('velo')
+    const velo = await screen.findByTestId('bar-chart-item-velo')
     // When
     act(() => {
       screen.getByLabelText('Voir tous les modes de transport').click()
@@ -50,15 +50,15 @@ describe('TransportPage - affiche la page itinéraire', () => {
     // Given
     mockRouter.push('/transport')
     renderWithWrapper(<TransportPage category={getTransportCategory()} />)
-    await screen.findByTestId('velo')
+    await screen.findByTestId('bar-chart-item-velo')
     // When
     act(() => {
       screen.getByLabelText('Afficher le covoiturage').click()
     })
     // Then
-    const voitureElectrique = await screen.queryAllByTestId('voitureelectrique')[0]
+    const voitureElectrique = await screen.queryAllByTestId('bar-chart-item-voitureelectrique')[0]
     expect(voitureElectrique.textContent).toBe('Voiture (moteur électrique)-avec 1 covoitureurs+0,5 kg CO2e')
-    const voitureElectriqueSeule = await screen.queryAllByTestId('voitureelectrique')[1]
+    const voitureElectriqueSeule = await screen.queryAllByTestId('bar-chart-item-voitureelectrique')[1]
     expect(voitureElectriqueSeule.textContent).toBe('Voiture (moteur électrique)1 kg CO2e')
   })
 })
