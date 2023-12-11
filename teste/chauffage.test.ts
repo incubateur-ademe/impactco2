fbb1ab5c95ef734ade99eb89459d10d0c315c588
David Boureau <bdavidxyz@gmail.com>
2023-12-11 14:39:48 +0100

    making all tests pass

--- teste/chauffage.test.ts
+++ teste/chauffage.test.ts
@@ -10,16 +10,16 @@ test('Chauffage page', async ({ page }) => {
 
   await test.step('Page is loaded with values for 63m2', async () => {
     await expect(page.getByTestId('slider-thumb-content')).toHaveText('63 m2')
-    await expect(page.getByTestId('bar-chart-item')).toHaveCount(7)
-    await expect(page.getByTestId('bar-chart-item-value').first()).toHaveText('246')
+    await expect(page.locator('.bar-chart-item')).toHaveCount(7)
+    await expect(page.locator('.bar-chart-item').first()).toHaveText('Chauffage avec une pompe à chaleur0246 kg CO2e')
   })
 
   await test.step('Values are updated with button', async () => {
     page.getByTestId('slider-plus-button').click()
 
     await expect(page.getByTestId('slider-thumb-content')).toHaveText('100 m2')
-    await expect(page.getByTestId('bar-chart-item')).toHaveCount(7)
-    await expect(page.getByTestId('bar-chart-item-value').first()).toHaveText('390')
+    await expect(page.locator('.bar-chart-item')).toHaveCount(7)
+    await expect(page.locator('.bar-chart-item').first()).toHaveText('Chauffage avec une pompe à chaleur0390 kg CO2e')
   })
 
   await test.step('Values are updated with manual entry', async () => {
@@ -28,7 +28,7 @@ test('Chauffage page', async ({ page }) => {
     page.getByTestId('slider-number-input-validate').click()
 
     await expect(page.getByTestId('slider-thumb-content')).toHaveText('200 m2')
-    await expect(page.getByTestId('bar-chart-item')).toHaveCount(7)
-    await expect(page.getByTestId('bar-chart-item-value').first()).toHaveText('780')
+    await expect(page.locator('.bar-chart-item')).toHaveCount(7)
+    await expect(page.locator('.bar-chart-item').first()).toHaveText('Chauffage avec une pompe à chaleur0780 kg CO2e')
   })
 })
