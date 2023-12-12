6855e752c62f205553278c8026a6d0fab7ea73d7
David Boureau <bdavidxyz@gmail.com>
2023-12-12 17:40:40 +0100

    fix: regression chauffage

--- teste/chauffage.test.ts
+++ teste/chauffage.test.ts
@@ -27,7 +27,7 @@ test('Chauffage page', async ({ page }) => {
     page.getByTestId('slider-number-input').fill('200')
     page.getByTestId('slider-number-input-validate').click()
 
-    await expect(page.getByTestId('slider-thumb-content')).toHaveText('100 m2')
+    await expect(page.getByTestId('slider-thumb-content')).toHaveText('200 m2')
     await expect(page.locator('.bar-chart-item')).toHaveCount(7)
     await expect(page.getByTestId('bar-chart-item-value').first()).toHaveText('780')
   })
