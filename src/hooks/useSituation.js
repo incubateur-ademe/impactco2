5158c2072cef5ff6b376ba8a5b096699db26c314
David Boureau <bdavidxyz@gmail.com>
2023-12-19 10:50:58 +0100

    refactor: no console log

--- src/hooks/useSituation.js
+++ src/hooks/useSituation.js
@@ -4,9 +4,7 @@ export default function useSituation(engine, defaultSituation) {
   const [localSituation, setLocalSituation] = useState(defaultSituation)
 
   const prevSituation = useRef(null)
-  console.log('prevSituation:', prevSituation)
 
-  console.log('localSituation:', localSituation)
   if (prevSituation.current !== localSituation) {
     const newSituation = { ...prevSituation.current, ...localSituation }
     prevSituation.current = newSituation
