b6b49f456ac01bd3f2de75d1423a642f67da34b3
David Boureau <bdavidxyz@gmail.com>
2023-12-19 17:33:54 +0100

    feat: works...

--- src/hooks/useSituation.js
+++ src/hooks/useSituation.js
@@ -14,36 +14,22 @@ function already(a, b) {
 }
 
 export default function useSituation(engine, defaultSituation) {
-  const [localSituation, setLocalSituation] = useState(defaultSituation)
+  const [bla, setBla] = useSessionStorage('usageNumerique', defaultSituation)
+  const [localSituation, setLocalSituation] = useState(bla)
 
-  const situation = useRef(null)
+  const prevSituation = useRef(null)
 
-  const [usageNumerique, setUsageNumerique] = useSessionStorage('usageNumerique', {
-    'email . appareil': `'smartphone'`,
-    'email . taille': 0.075,
-    'email . transmission . émetteur . réseau': `'fixe FR'`,
-    'streaming . durée': 420,
-    'streaming . appareil': `'TV'`,
-    'streaming . qualité': `'HD'`,
-    'streaming . transmission . réseau': `'fixe FR'`,
-    'visio . appareil': `'ordinateur portable'`,
-    'visio . qualité': `'SD'`,
-    'visio . durée': 180,
-    'visio . transmission . réseau': `'fixe FR'`,
-    'visio . emplacements': 1,
-  })
-
-  if (!already(situation.current, localSituation)) {
-    const newSituation = { ...usageNumerique, ...localSituation }
-    if (localSituation) {
-      setUsageNumerique(newSituation)
-    }
-    situation.current = newSituation
+  if (!already(prevSituation.current, localSituation)) {
+    console.log('localSituation:', localSituation)
+    const newSituation = { ...prevSituation.current, ...localSituation }
+    console.log('newSituation:', newSituation)
+    prevSituation.current = newSituation
     engine && engine.setSituation(newSituation)
+    if (typeof window !== 'undefined') setBla(newSituation)
   }
 
   return {
-    situation: situation.current,
+    situation: prevSituation.current,
     setSituation: setLocalSituation,
   }
 }
