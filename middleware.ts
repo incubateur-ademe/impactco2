5e105af17bb225e15d3cc954ceef81ab8b52d096
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-12-19 13:24:12 +0100

    fix: redirection

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  console.log(request.nextUrl)
+  if (request.nextUrl.hostname.includes('monconvertisseurco2.fr')) {
+    if (request.nextUrl.pathname === 'iframe.js') {
+      return NextResponse.redirect('https://impactco2.fr/iframe.js')
+    }
+    return NextResponse.redirect('https://impactco2.fr/comparateur')
+  }
+}
