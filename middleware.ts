9ab53b9f4293fe866c7445c56ae4503a2aa1b1c6
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-12-20 15:34:41 +0100

    chore: change console.log

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl)
+  console.log(request.url)
+  console.log(request.referrer)
   if (request.nextUrl.hostname.includes('monconvertisseurco2.fr')) {
     if (request.nextUrl.pathname === 'iframe.js') {
       return NextResponse.redirect('https://impactco2.fr/iframe.js')
