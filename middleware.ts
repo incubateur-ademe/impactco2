014e4a5eb015b77bfcd8af3bf782867fe6c90d15
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-12-20 16:42:18 +0100

    chore: fix monconvertisseur iframe

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(request: NextRequest) {
   const host = request.headers.get('host')
   if (host === 'monconvertisseurco2.fr') {
-    if (request.nextUrl.pathname === 'iframe.js') {
+    if (request.nextUrl.pathname === '/iframe.js') {
       return NextResponse.redirect('https://impactco2.fr/iframe.js')
     }
     return NextResponse.redirect('https://impactco2.fr/comparateur')
