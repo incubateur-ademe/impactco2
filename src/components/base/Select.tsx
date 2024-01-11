2c37ab0600677f9d22106534e1678a8da3003d8f
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-01-11 18:01:40 +0100

    fix: Email visualisation

--- src/components/base/Select.tsx
+++ src/components/base/Select.tsx
@@ -13,7 +13,7 @@ const Input = styled.select<{ $color?: string }>`
   background-color: transparent;
   background-image: ${({ $color }) =>
     $color
-      ? `url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='${$color.replace(
+      ? `url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23${$color.replace(
           '#',
           ''
         )}' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e")`
