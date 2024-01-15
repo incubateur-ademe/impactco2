97c27b18cf32d32c43a8fbfe8334be0dad511b89
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-01-15 14:31:50 +0100

    chore: Add CSP (#587)

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,12 +1,12 @@
 import * as Sentry from '@sentry/nextjs'
 
 export const initializeSentry = () => {
-  if (!process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.NODE_ENV !== 'production') {
+  if (!process.env.NEXT_PUBLIC_SENTRY_DNS || process.env.NODE_ENV !== 'production') {
     return
   }
 
   Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
     environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
     tracesSampleRate: 0.01,
   })
