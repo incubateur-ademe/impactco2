985208f4e6566f878c0e3d8855afaa31ef203632
David Boureau <bdavidxyz@gmail.com>
2024-01-29 18:11:44 +0100

    feat: YearlyLivraison.test.js

--- testc/Seo.test.tsx
+++ testc/Seo.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import mockRouter from 'next-router-mock'
+import { ReactNode } from 'react'
 import Seo from 'components/layout/web/Seo'
 
 jest.mock('next/router', () => jest.requireActual('next-router-mock'))
@@ -8,7 +9,7 @@ jest.mock('next/router', () => jest.requireActual('next-router-mock'))
 jest.mock('next/head', () => {
   return {
     __esModule: true,
-    default: ({ children }) => {
+    default: ({ children }: { children: ReactNode }) => {
       return <>{children}</>
     },
   }
@@ -37,49 +38,63 @@ describe('Seo', () => {
   })
   it('La meta image est /metaimage.png par défaut', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name=image]')[0].content).toBe('https://example.com/metaimage.png')
+    expect((container.querySelectorAll('meta[name=image]')[0] as HTMLMetaElement).content).toBe(
+      'https://example.com/metaimage.png'
+    )
   })
   it('La meta description est "sensibilisez..." par défaut', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name=description]')[0].content).toBe(default_description)
+    expect((container.querySelectorAll('meta[name=description]')[0] as HTMLMetaElement).content).toBe(
+      default_description
+    )
   })
   it("La meta og:url représente bien l'URL courante (version simple)", () => {
     mockRouter.push('/current-path')
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[property="og:url"]')[0].content).toBe('https://example.com/current-path')
+    expect((container.querySelectorAll('meta[property="og:url"]')[0] as HTMLMetaElement).content).toBe(
+      'https://example.com/current-path'
+    )
   })
   it("La meta og:url représente bien l'URL courante (version compliquée)", () => {
     mockRouter.push('/another/a?b=c')
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[property="og:url"]')[0].content).toBe('https://example.com/another/a?b=c')
+    expect((container.querySelectorAll('meta[property="og:url"]')[0] as HTMLMetaElement).content).toBe(
+      'https://example.com/another/a?b=c'
+    )
   })
   it('La meta og:title vaut la même chose que le title', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[property="og:title"]')[0].content).toBe(default_title)
+    expect((container.querySelectorAll('meta[property="og:title"]')[0] as HTMLMetaElement).content).toBe(default_title)
   })
   it('La meta og:description vaut la même chose que la description', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[property="og:description"]')[0].content).toBe(default_description)
+    expect((container.querySelectorAll('meta[property="og:description"]')[0] as HTMLMetaElement).content).toBe(
+      default_description
+    )
   })
   it('La meta twitter:card vaut la constante summary_large_image', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name="twitter:card"]')[0].content).toBe('summary_large_image')
+    expect((container.querySelectorAll('meta[name="twitter:card"]')[0] as HTMLMetaElement).content).toBe(
+      'summary_large_image'
+    )
   })
   it('La meta twitter:creator est pour le compte ademe', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name="twitter:creator"]')[0].content).toBe('ademe')
+    expect((container.querySelectorAll('meta[name="twitter:creator"]')[0] as HTMLMetaElement).content).toBe('ademe')
   })
   it('La meta twitter:title vaut la même chose que le titre', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name="twitter:title"]')[0].content).toBe(default_title)
+    expect((container.querySelectorAll('meta[name="twitter:title"]')[0] as HTMLMetaElement).content).toBe(default_title)
   })
   it('La meta twitter:description vaut la même chose que la description', () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name="twitter:description"]')[0].content).toBe(default_description)
+    expect((container.querySelectorAll('meta[name="twitter:description"]')[0] as HTMLMetaElement).content).toBe(
+      default_description
+    )
   })
   it("La meta twitter:image vaut la même chose que l'image", () => {
     const { container } = render(<Seo />)
-    expect(container.querySelectorAll('meta[name="twitter:image"]')[0].content).toBe(
+    expect((container.querySelectorAll('meta[name="twitter:image"]')[0] as HTMLMetaElement).content).toBe(
       'https://example.com/metaimage.png'
     )
   })
@@ -89,10 +104,12 @@ describe('Seo', () => {
   })
   it('La meta image peut être personnalisée', () => {
     const { container } = render(<Seo image={'myimage.png'} />)
-    expect(container.querySelectorAll('meta[name=image]')[0].content).toBe('https://example.com/myimage.png')
+    expect((container.querySelectorAll('meta[name=image]')[0] as HTMLMetaElement).content).toBe(
+      'https://example.com/myimage.png'
+    )
   })
   it('La description peut être personnalisée', () => {
     const { container } = render(<Seo description={'my description'} />)
-    expect(container.querySelectorAll('meta[name=description]')[0].content).toBe('my description')
+    expect((container.querySelectorAll('meta[name=description]')[0] as HTMLMetaElement).content).toBe('my description')
   })
 })
