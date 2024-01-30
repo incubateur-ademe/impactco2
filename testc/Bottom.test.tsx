530529f792a315d04751e5d8acd19f3be208f229
David Boureau <bdavidxyz@gmail.com>
2024-01-30 13:20:50 +0100

    feat: ok for category

--- testc/Bottom.test.tsx
+++ testc/Bottom.test.tsx
@@ -1,8 +1,12 @@
 import '@testing-library/jest-dom'
 import { Screen, screen } from '@testing-library/react'
 import { Category } from 'types/category'
+import categories from 'data/categories.json'
 import { renderWithWrapper } from '../test-utils/render-with-wrapper.js'
 import Bottom from 'components/misc/category/Bottom'
+
+// Take a random category
+const category = categories.find((e) => e.id === 4) as Category
 
 describe('Bottom - Texte et bouton affichés en bas de chaque thématique', () => {
   // See https://webtips.dev/how-to-mock-processenv-in-jest
@@ -19,7 +23,7 @@ describe('Bottom - Texte et bouton affichés en bas de chaque thématique', () =
 
   test('Affiche un texte et un bouton par défaut', async () => {
     // When
-    renderWithWrapper(<Bottom category={{} as Category} />)
+    renderWithWrapper(<Bottom category={category} />)
     // Then
     expect(await screen.findByTestId('bottomText')).toBeInTheDocument()
     expect(await screen.findByTestId('bottomText')).toHaveTextContent('Valeurs exprimées en kg CO2e émis')
@@ -27,19 +31,19 @@ describe('Bottom - Texte et bouton affichés en bas de chaque thématique', () =
   })
   test("Le bouton a bien le texte 'Voir toutes les thématiques'", async () => {
     // When
-    renderWithWrapper(<Bottom category={{} as Category} />)
+    renderWithWrapper(<Bottom category={category} />)
     // Then
     expect(await actionButton(screen)).toHaveTextContent('Voir toutes les thématiques')
   })
   test('Le bouton redirige bien vers /thematiques par défaut', async () => {
     // When
-    renderWithWrapper(<Bottom category={{} as Category} />)
+    renderWithWrapper(<Bottom category={category} />)
     // Then
     expect(await actionButton(screen)).toHaveAttribute('href', '/thematiques')
   })
   test('Le bouton redirige bien vers https://example.com/thematiques si la propriété iframe est présente', async () => {
     // When
-    renderWithWrapper(<Bottom category={{} as Category} iframe />)
+    renderWithWrapper(<Bottom category={category} iframe />)
     // Then
     expect(await actionButton(screen)).toHaveAttribute('href', 'https://example.com/thematiques')
   })
