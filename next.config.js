ca62b087700a951e81b057cc0402421cc51e7f82
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-02-06 11:32:21 +0100

    fix: Sentry configuration and back errors

--- next.config.js
+++ next.config.js
@@ -164,7 +164,14 @@ const nextConfig = {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options.
 const sentryWebpackPluginOptions = {
-  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+  org: 'betagouv',
+  project: 'impact-co2',
+  url: 'https://sentry.io/',
 }
 
 module.exports = (phase) => {
@@ -172,5 +179,31 @@ module.exports = (phase) => {
     console.log('Current shortSha is: ', getShortSha(getLocalGitCommitHash()))
   }
 
-  return withBundleAnalyzer(withSentryConfig(nextConfig, sentryWebpackPluginOptions))
+  return withBundleAnalyzer(
+    withSentryConfig(nextConfig, sentryWebpackPluginOptions, {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: '/monitoring',
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+
+      // Enables automatic instrumentation of Vercel Cron Monitors.
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true,
+    })
+  )
 }
