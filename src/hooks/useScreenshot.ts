df548aec9e18d239fcaf6dfde576b2f4c319635f
David Boureau <bdavidxyz@gmail.com>
2024-02-20 19:24:47 +0100

    refactor: using useTheme

--- src/hooks/useScreenshot.ts
+++ src/hooks/useScreenshot.ts
@@ -1,22 +1,20 @@
 import { toJpeg, toPng } from 'html-to-image'
-import { useSearchParams } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
 import { track } from 'utils/matomo'
+import useTheme from 'components/layout/Theme'
 
 export default function useScreenshot(slug: string, tracking: string, format: string = 'png') {
   const ref = useRef<HTMLDivElement>(null)
-  const searchParams = useSearchParams()
 
-  const theme = searchParams.get('theme')
+  const { theme } = useTheme()
   const transformFn = format === 'png' ? toPng : toJpeg
 
   const [isScreenshotting, setIsScreenshotting] = useState(false)
-  const isDarkMode = theme === 'night'
   useEffect(() => {
     if (isScreenshotting && ref.current !== null) {
       transformFn(ref.current, {
         cacheBust: true,
-        backgroundColor: isDarkMode ? 'black' : 'white', // variables CSS non disponibles au moment du screenshot
+        backgroundColor: theme === 'night' ? 'black' : 'white', // variables CSS non disponibles au moment du screenshot
         filter: (node) => {
           return !node.className || !node.className.includes ? true : !node.className?.includes('noscreenshot')
         },
