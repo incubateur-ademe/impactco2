2e856bf332a53d4479a7fd3117ca41abafaba1f4
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-03-27 11:30:50 +0100

    feat: Add information on script (#643)
    * feat: Add information on script
    
    * fix:

--- src/components/base/ClipboardBox.tsx
+++ src/components/base/ClipboardBox.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from 'react'
 import { track } from 'utils/matomo'
 import { Icon } from '../osezchanger/icons'
-import { Box, Content, Copy } from './ClipboardBox.styles'
+import { Box, Content, Copy, Information } from './ClipboardBox.styles'
 
 const ClipboardBox = ({ children, colored, tracking }: { children: string; colored?: boolean; tracking: string }) => {
   const [copied, setCopied] = useState(false)
   return (
-    <Box
-      $colored={colored}
-      onClick={() => {
-        setCopied(true)
-        setTimeout(() => setCopied(false), 500)
-        navigator.clipboard.writeText(children)
-        track(tracking, 'Copy', children)
-      }}>
-      <Content data-testid='clipboard-box'>{children}</Content>
-      <Copy className='clipboard-right-item' $copied={copied}>
-        {copied ? 'Copié' : 'Copier'}
-        <Icon iconId={copied ? 'check' : 'copy'} />
-      </Copy>
-    </Box>
+    <>
+      <Box
+        $colored={colored}
+        onClick={() => {
+          setCopied(true)
+          setTimeout(() => setCopied(false), 500)
+          navigator.clipboard.writeText(children)
+          track(tracking, 'Copy', children)
+        }}>
+        <Content data-testid='clipboard-box'>{children}</Content>
+        <Copy className='clipboard-right-item' $copied={copied}>
+          {copied ? 'Copié' : 'Copier'}
+          <Icon iconId={copied ? 'check' : 'copy'} />
+        </Copy>
+      </Box>
+      {children.startsWith('<script') && (
+        <Information>
+          <Icon iconId='information' />
+          Cette balise de code vous permet d'afficher le simulateur à l'intérieur d'une page de votre site internet.
+          <br /> Le module sera automatiquement mis à jour avec les dernières nouveautés.
+        </Information>
+      )}
+    </>
   )
 }
 
