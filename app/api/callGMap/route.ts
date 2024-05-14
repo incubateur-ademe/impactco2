fe1a11515f6e88aecbe3e23707f42ad67b1d8aa0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-05-14 12:01:39 +0200

    remove api limiter

--- app/api/callGMap/route.ts
+++ app/api/callGMap/route.ts
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import { ValueDeterminingMiddleware, rateLimit } from 'express-rate-limit'
-import slowDown from 'express-slow-down'
 import { NextRequest, NextResponse } from 'next/server'
 import { GMapValidation, getCachedValue, insertCachedValue } from 'utils/gmaps'
 import { trackAPIRequest } from 'utils/middleware'
@@ -17,28 +15,6 @@ const getValue = (distance: GMapDistances) => {
   return (element.status === 'OK' && element.distance.value / 1000) || 0
 }
 
-// Largement inspiré de https://kittygiraudel.com/2022/05/16/rate-limit-nextjs-api-routes/
-const getIP: ValueDeterminingMiddleware<string> = (request) =>
-  (request.ip ||
-    request.headers['x-forwarded-for'] ||
-    request.headers['x-real-ip'] ||
-    request.socket.remoteAddress) as string
-
-// Pour info, on fait les appels 3 par 3 (mode driving, walking et transit)
-// Donc on limit à 30 appels par minutes, et on ralentit au bout de la 10 eme de 100ms
-const getRateLimitMiddlewares = ({ limit = 30, windowMs = 60 * 1000, delayAfter = 10, delayMs = 100 } = {}) => [
-  slowDown({ keyGenerator: getIP, windowMs, delayAfter, delayMs: () => delayMs }),
-  rateLimit({ keyGenerator: getIP, windowMs, max: limit }),
-]
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-const applyMiddleware = (middleware: Function) => (req: NextRequest) =>
-  new Promise((resolve, reject) => {
-    middleware(req, NextResponse, (result: unknown) => (result instanceof Error ? reject(result) : resolve(result)))
-  })
-
-const middlewares = getRateLimitMiddlewares().map(applyMiddleware)
-
 export async function POST(req: NextRequest) {
   const body = await req.json()
 
@@ -47,16 +23,11 @@ export async function POST(req: NextRequest) {
     return NextResponse.json(inputs.error)
   }
 
-  if (process.env.LIMIT_API) {
+  if (process.env.LIMIT_API === 'true') {
     const referer = req.headers.get('referer')
 
     if (!process.env.NEXT_PUBLIC_URL || !referer?.startsWith(process.env.NEXT_PUBLIC_URL)) {
       return NextResponse.json('Not authorized', { status: 403 })
-    }
-    try {
-      await Promise.all(middlewares.map((middleware) => middleware(req)))
-    } catch {
-      return NextResponse.json('Too Many Requests', { status: 420 })
     }
   }
 
