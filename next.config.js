2a31c685f47995b18197d4b73305b23da74ed472
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-05-27 11:28:57 +0200

    fix: sentry

--- next.config.js
+++ next.config.js
@@ -78,13 +78,6 @@ const nextConfig = {
   images: {
     remotePatterns: [images],
   },
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-  sentry: {
-    autoInstrumentServerFunctions: true,
-    autoInstrumentMiddleware: true,
-    widenClientFileUpload: true,
-    hideSourceMaps: false,
-  },
   async headers() {
     return [
       {
@@ -169,31 +162,4 @@ const sentryWebpackPluginOptions = {
   url: process.env.SENTRY_URL,
 }
 
-module.exports = () =>
-  withBundleAnalyzer(
-    withSentryConfig(nextConfig, sentryWebpackPluginOptions, {
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Transpiles SDK to be compatible with IE11 (increases bundle size)
-      transpileClientSDK: true,
-
-      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-      tunnelRoute: '/monitoring',
-
-      // Hides source maps from generated client bundles
-      hideSourceMaps: true,
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors.
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    })
-  )
+module.exports = () => withBundleAnalyzer(withSentryConfig(nextConfig, sentryWebpackPluginOptions))
