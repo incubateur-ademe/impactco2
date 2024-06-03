85f5505a9f0b42e206c498e370c783e626947388
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-06-03 16:16:09 +0200

    proper url

--- src/components/shareable/overScreens/Share.tsx
+++ src/components/shareable/overScreens/Share.tsx
@@ -108,7 +108,7 @@ const Share = ({
             src={
               category
                 ? `/meta/${category.slug}.png`
-                : `${process.env.NEXT_PUBLIC_IMAGE_URL}/api/dynamics/${path}?${buildCustomParamsUrl(params, visibility)}`
+                : `${process.env.NEXT_PUBLIC_IMAGE_URL}/api/dynamics/comparateur?${buildCustomParamsUrl(params, visibility)}`
             }
             width={728}
             height={382.2}
