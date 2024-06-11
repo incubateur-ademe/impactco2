680210de624318bf3eb656716a8d645622dedade
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-06-11 10:30:28 +0200

    fix: detector image size

--- src/components/base/EquivalentIcon.tsx
+++ src/components/base/EquivalentIcon.tsx
@@ -6,10 +6,6 @@ const EquivalentIcon = ({ equivalent, height }: { equivalent: Pick<ComputedEquiv
   return (
     <img
       src={buildCurrentUrlFor(`/icons/${equivalent.slug.endsWith('courrier') ? 'avion' : equivalent.slug}.svg`)}
-      style={{
-        width: `${height || 1}rem !important`,
-        height: `${height || 1}rem !important`,
-      }}
       width={(height || 1) * 16}
       height={(height || 1) * 16}
       alt=''
