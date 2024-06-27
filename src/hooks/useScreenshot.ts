ef263a31f860a05ff342033fd8fc05735bff14a6
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-06-27 15:52:10 +0200

    remove html-to-image

--- src/hooks/useScreenshot.ts
+++ src/hooks/useScreenshot.ts
@@ -1,36 +1,39 @@
 'use client'
 
-import { toJpeg, toPng } from 'html-to-image'
 import { useEffect, useRef, useState } from 'react'
 import { track } from 'utils/matomo'
 import useTheme from 'components/layout/UseTheme'
 
-export default function useScreenshot(slug: string, tracking: string, format: string = 'png') {
+export default function useScreenshot(slug: string, tracking: string) {
   const ref = useRef<HTMLDivElement>(null)
 
   const { theme } = useTheme()
-  const transformFn = format === 'png' ? toPng : toJpeg
 
   const [isScreenshotting, setIsScreenshotting] = useState(false)
   useEffect(() => {
-    if (isScreenshotting && ref.current !== null) {
-      transformFn(ref.current, {
-        cacheBust: true,
-        backgroundColor: theme === 'night' ? 'black' : 'white', // variables CSS non disponibles au moment du screenshot
-      })
-        .then((dataUrl) => {
-          const link = document.createElement('a')
-          link.download = `${slug}.${format}`
-          link.href = dataUrl
-          link.click()
+    if (isScreenshotting) {
+      import('html-to-image').then((mod) => {
+        if (ref.current !== null) {
+          mod
+            .toPng(ref.current, {
+              cacheBust: true,
+              backgroundColor: theme === 'night' ? 'black' : 'white', // variables CSS non disponibles au moment du screenshot
+            })
+            .then((dataUrl) => {
+              const link = document.createElement('a')
+              link.download = `${slug}.png`
+              link.href = dataUrl
+              link.click()
 
-          setIsScreenshotting(false)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+              setIsScreenshotting(false)
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        }
+      })
     }
-  }, [ref, isScreenshotting, format, slug, transformFn])
+  }, [ref, isScreenshotting, slug])
 
   const takeScreenshot = () => {
     track(tracking, 'Screenshot', slug)
