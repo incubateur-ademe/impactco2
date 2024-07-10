6e8cbab35f493bce1325e128d2dcfa10baa705dd
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-07-10 09:14:59 +0200

    feat: Add logo on infography (#692)

--- src/components/outils/equivalents/infographies/InfographySimulator.tsx
+++ src/components/outils/equivalents/infographies/InfographySimulator.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, useMemo } from 'react'
 import { computedEquivalents } from 'src/providers/equivalents'
 import { ComputedEquivalent } from 'types/equivalent'
 import EquivalentIcon from 'components/base/EquivalentIcon'
+import Logos from 'components/base/Logo/Logos'
 import EqualIcon from 'components/base/icons/equal'
 import styles from './InfographySimulator.module.css'
 import Name from './Name'
@@ -25,26 +26,32 @@ const InfographySimulator = ({ equivalents }: { equivalents: string[] }) => {
   const factor = values[0].value
   return (
     <div className={styles.infography} id='infographie'>
-      {values.map((value) => {
-        const number = Math.round(factor / value.value)
-        const height = number > 2000 ? 0.5 : number > 50 ? 1.5 : 3
-        const gap = number > 2000 ? 0.125 : number > 50 ? 0.5 : 0.75
-        return (
-          <Fragment key={value.slug}>
-            <div className={styles.equivalent}>
-              <div className={styles.icons} style={{ gap: `${gap}rem` }}>
-                {[...Array(number)].map((number) => (
-                  <EquivalentIcon key={`${value.slug}-${number}`} equivalent={value} height={height} />
-                ))}
+      <div>
+        {values.map((value) => {
+          const number = Math.round(factor / value.value)
+          const height = number > 2000 ? 0.5 : number > 50 ? 1.5 : 3
+          const gap = number > 2000 ? 0.125 : number > 50 ? 0.5 : 0.75
+          return (
+            <Fragment key={value.slug}>
+              <div className={styles.equivalent}>
+                <div className={styles.icons} style={{ gap: `${gap}rem` }}>
+                  {[...Array(number)].map((number) => (
+                    <EquivalentIcon key={`${value.slug}-${number}`} equivalent={value} height={height} />
+                  ))}
+                </div>
+                <Name equivalent={value} value={number} />
               </div>
-              <Name equivalent={value} value={number} />
-            </div>
-            <div className={styles.equal}>
-              <EqualIcon />
-            </div>
-          </Fragment>
-        )
-      })}
+              <div className={styles.equal}>
+                <EqualIcon />
+              </div>
+            </Fragment>
+          )
+        })}
+      </div>
+      <div className={styles.disclaimer}>Comparaison basée sur la quantité de kg CO2e émise.</div>
+      <div className={styles.logos}>
+        <Logos small />
+      </div>
     </div>
   )
 }
