e4d37d3fb77b2e124be35a7a6248095c99e600f0
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-08-19 15:20:19 +0200

    remove logo

--- teste/detecteur-co2.spec.ts
+++ teste/detecteur-co2.spec.ts
@@ -7,9 +7,9 @@ test('Display detector', async ({ page }) => {
   await page.getByRole('button', { name: '100 kg de CO₂e' }).click()
 
   await expect(page.getByRole('alertdialog')).toBeVisible()
-  await expect(page.getByLabel('Logo Impact CO2')).toHaveAttribute('href', 'https://impactco2.fr/comparateur?value=100')
-  await expect(page.getByLabel('Logo Impact CO2')).toHaveAttribute('target', '_blank')
-  await expect(page.getByLabel('Logo Impact CO2')).toHaveAttribute('rel', 'noreferrer noopener')
+  await expect(page.getByLabel('Impact CO2')).toHaveAttribute('href', 'https://impactco2.fr/comparateur?value=100')
+  await expect(page.getByLabel('Impact CO2')).toHaveAttribute('target', '_blank')
+  await expect(page.getByLabel('Impact CO2')).toHaveAttribute('rel', 'noreferrer noopener')
 
   const value = await page.locator('.impactCO2-etiquette-value').textContent()
 
