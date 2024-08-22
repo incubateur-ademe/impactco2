c43610ef29b800a407296813204f05e9d936b2be
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-08-22 10:09:08 +0200

    fix: always display result in tt

--- src/components/outils/TeletravailSimulator.tsx
+++ src/components/outils/TeletravailSimulator.tsx
@@ -129,7 +129,7 @@ const TeletravailSimulator = () => {
           </div>
         </div>
       </div>
-      {start && end && itineraries && total ? (
+      {start && end && itineraries ? (
         <>
           <div className={styles.results}>
             <div className={styles.values}>
