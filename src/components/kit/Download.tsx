72ad0c9da8918006b5a83a710a7e2942dc61fd43
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-09-11 14:18:21 +0200

    feat: plaquette

--- src/components/kit/Download.tsx
+++ src/components/kit/Download.tsx
@@ -4,10 +4,13 @@ import React from 'react'
 import { track } from 'utils/matomo'
 import Link from 'components/base/buttons/Link'
 
-const Download = ({ name }: { name: string }) => {
+const Download = ({ name, customLabel, customHref }: { name: string; customLabel?: string; customHref?: string }) => {
   return (
-    <Link asButton href={`/kit/${name}.zip`} onClick={() => track('Kit de communication', name, 'download-zip')}>
-      Télécharger les {name} (.zip)
+    <Link
+      asButton
+      href={customHref || `/kit/${name}.zip`}
+      onClick={() => track('Kit de communication', name, 'download-zip')}>
+      {customLabel || 'Télécharger les {name} (.zip)'}
     </Link>
   )
 }
