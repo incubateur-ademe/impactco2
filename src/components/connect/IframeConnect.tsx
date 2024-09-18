0772155c6cab45c43ccff462c1b2b08bbb8bcdfc
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-09-18 10:55:29 +0200

    feat: Add connect iframe loader (#730)

--- src/components/connect/IframeConnect.tsx
+++ src/components/connect/IframeConnect.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import LoadingIcon from 'components/base/icons/loading'
 import styles from './IframeConnect.module.css'
 
 const IframeConnect = ({ src, title }: { src?: string; title: string }) => {
   const ref = useRef<HTMLIFrameElement>(null)
+  const [ready, setReady] = useState(false)
   useEffect(() => {
     if (ref.current) {
       const resize = function (e: MessageEvent) {
         if (ref.current && e.data.s === ref.current.src) {
           if (e.data && e.data.t === 'h') {
             ref.current.style.height = e.data.h + 'px'
+            setReady(true)
           }
         }
       }
@@ -19,7 +22,16 @@ const IframeConnect = ({ src, title }: { src?: string; title: string }) => {
     }
   }, [ref])
 
-  return <iframe ref={ref} id='iframeUAT' title={title} className={styles.iframe} src={src} />
+  return (
+    <>
+      {!ready && (
+        <div className={styles.loading}>
+          Chargement du formulaire en cours <LoadingIcon />
+        </div>
+      )}
+      <iframe ref={ref} id='iframeUAT' title={title} className={ready ? styles.iframe : styles.hidden} src={src} />
+    </>
+  )
 }
 
 export default IframeConnect
