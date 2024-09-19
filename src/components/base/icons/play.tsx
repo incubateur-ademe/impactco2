4ef4ee2dc476ca6c2b242338e4aa6af06d060901
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-09-19 10:21:55 +0200

    feat: disclaimer on the etiquette (#724)

--- src/components/base/icons/play.tsx
+++ src/components/base/icons/play.tsx
@@ -4,7 +4,7 @@ const PlayIcon = () => (
   <svg aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12' fill='none'>
     <path
       d='M9.563 4.7918C10.2767 5.16639 10.2767 6.18811 9.563 6.5627L4.46474 9.23859C3.79887 9.58808 3 9.10515 3 8.35314V3.00136C3 2.24935 3.79887 1.76642 4.46474 2.11591L9.563 4.7918Z'
-      fill='#6A5F66'
+      fill='currentcolor'
     />
   </svg>
 )
