08e399710072138f59927de4ae57f4a7f23d422d
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-11-28 11:00:10 +0100

    feat: add images

--- src/components/kit/KitCommunication.tsx
+++ src/components/kit/KitCommunication.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import Image from 'next/image'
-import React from 'react'
 import outilStyles from '../outils/Outil.module.css'
 import Ademe from 'components/base/Logo/Ademe'
 import Logo from 'components/base/Logo/ImpactCO2'
@@ -31,14 +30,14 @@ const KitCommunication = () => {
           </div>
           <div className={outilStyles.content}>
             <h2>Logos</h2>
-            <span>
+            <p>
               Si vous souhaitez utiliser le logo d’Impact CO₂ pour communiquer sur vos propres supports (par exemple,
               pour évoquer votre utilisation de nos outils), vous pouvez utiliser le bloc marque.
-            </span>
-            <span>
+            </p>
+            <p>
               Le bloc marque comprend les logos de la République Française, de l’Ademe et d’Impact CO₂, tels que nous
               les utilisons sur nos propres supports.
-            </span>
+            </p>
           </div>
           <div className={outilStyles.link}>
             <Download name='logos' />
@@ -52,15 +51,15 @@ const KitCommunication = () => {
           </div>
           <div className={outilStyles.content}>
             <h2>Collection d’images</h2>
-            <span>
+            <p>
               Nous mettons à votre disposition une collection d’images dédiées à chacun des outils disponibles sur notre
               site web et présentant le concept du site en une image synthétique.
-            </span>
-            <span>
+            </p>
+            <p>
               Si vous souhaitez communiquer sur l’utilisation de nos ressources, nous avons prévu un emplacement sur
               chacune de ces images pour placer votre logo.
-            </span>
-            <span>Pour obtenir les images, cliquez sur le bouton ci-dessous :</span>
+            </p>
+            <p>Pour obtenir les images, cliquez sur le bouton ci-dessous :</p>
           </div>
           <div className={outilStyles.link}>
             <Download name='images' />
@@ -74,21 +73,21 @@ const KitCommunication = () => {
           </div>
           <div className={outilStyles.content}>
             <h2>Illustrations</h2>
-            <span>
+            <p>
               Si vous le souhaitez, vous pouvez utiliser les icônes que nous avons créées dans vos propres infographies,
               pour illustrer l’impact sur le climat des gestes et objets du quotidien.
-            </span>
-            <span>
+            </p>
+            <p>
               En échange, nous vous demandons de bien vouloir créditer Impact CO₂ en tant qu’auteur des illustrations,
               avec un lien vers notre site web. Merci !
-            </span>
-            <span>
+            </p>
+            <p>
               Exemple de citation : "illustrations réalisées par{' '}
               <Link href='#' internal>
                 Impact CO₂
               </Link>
               "
-            </span>
+            </p>
           </div>
           <div className={outilStyles.link}>
             <Download name='illustrations' />
@@ -102,11 +101,11 @@ const KitCommunication = () => {
           </div>
           <div className={outilStyles.content}>
             <h2>Plaquette de présentation</h2>
-            <span>
+            <p>
               Nous mettons à votre disposition une courte plaquette de présentation pour pouvoir partager facilement
               notre service public à votre entourage professionnel. N’hésitez pas à l’utiliser lors de vos prochains
               ateliers, ou à le diffuser dans vos canaux de communication !
-            </span>
+            </p>
           </div>
           <div className={outilStyles.link}>
             <Download name='plaquette' customHref='/kit/plaquette.pdf' customLabel='Télécharger la plaquette (.pdf)' />
@@ -120,17 +119,39 @@ const KitCommunication = () => {
           </div>
           <div className={outilStyles.content}>
             <h2>Kit de com’ pour le Quiz carbone</h2>
-            <span>
+            <p>
               Afin de vous faciliter la tâche, nous avons concocté des visuels prêts à l’emploi pour communiquer autour
               du Quiz carbone.
-            </span>
-            <span>
+            </p>
+            <p>
               Le kit inclut des exemples de posts pour les réseaux sociaux, ainsi que des visuels personnalisables avec
-              votre logo, ou non, à différents formats (horizontal, carré, vertical, bannière email).{' '}
-            </span>
+              votre logo, ou non, à différents formats (horizontal, carré, vertical, bannière email).
+            </p>
           </div>
           <div className={outilStyles.link}>
             <Download name='kit' customHref='/kit/quiz.zip' customLabel='Télécharger le kit (.zip)' />
+          </div>
+        </Card>
+      </Block>
+      <Block>
+        <Card className={outilStyles.card}>
+          <div className={classNames(outilStyles.image, styles.image)}>
+            <Image src='/images/banner-kit-alimentation.png' width={748} height={180} alt='' />
+          </div>
+          <div className={outilStyles.content}>
+            <h2>Kit de com’ pour Impact Alimentation</h2>
+            <p>
+              Nous avons préparé des visuels prêts à l’emploi pour vous permettre de communiquer facilement sur Impact
+              Alimentation.
+            </p>
+            <p>
+              Comme pour le Quiz, ce kit inclut des exemples de posts pour les réseaux sociaux, ainsi que des visuels
+              personnalisables avec votre logo, ou non, à différents formats (horizontal, carré, vertical, bannière
+              email).
+            </p>
+          </div>
+          <div className={outilStyles.link}>
+            <Download name='kit' customHref='/kit/alimentation.zip' customLabel='Télécharger le kit (.zip)' />
           </div>
         </Card>
       </Block>
