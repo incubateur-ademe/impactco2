d7bd7d5e823f7b05fbb60a8a5127d116c13f1643
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-12-16 13:58:10 +0100

    fix: Etiquette with no value (#762)

--- shopify/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { render } from 'preact/compat'
-import Shopify from '../src/components/externalModules/shopify/ShopifyEquivalent'
-
-const shopify = React.createElement(Shopify, null)
-render(shopify, document.getElementById('container') as HTMLElement)
