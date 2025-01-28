4c6915d45c12e8756b68cffb9f1785680cb5e3e7
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-01-28 16:55:33 +0100

    fix: 404 and 500 accessibility

--- src/views/NotFoundPage.tsx
+++ src/views/NotFoundPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Link from 'components/base/buttons/Link'
 import Block from 'components/layout/Block'
 import styles from './ErrorPage.module.css'
@@ -7,10 +6,8 @@ const NotFoundPage = () => {
   return (
     <div className='main-container'>
       <Block as='h1' title='Page non trouvée' description='Erreur 404'>
-        <div className={styles.bold}>
-          La page que vous cherchez est introuvable. Excusez-nous pour la gêne occasionnée.
-        </div>
-        <div className={styles.text}>
+        <p className={styles.bold}>La page que vous cherchez est introuvable. Excusez-nous pour la gêne occasionnée.</p>
+        <p className={styles.text}>
           Si vous avez tapé l’adresse web dans le navigateur, vérifiez qu’elle est correcte. La page n’est peut-être
           plus disponible.
           <br />
@@ -18,9 +15,9 @@ const NotFoundPage = () => {
           Dans ce cas, pour continuer votre visite vous pouvez consulter notre <Link href='/'>page d’accueil</Link>,
           sinon vous pouvez <Link href='/rendez-vous?fromLabel=404'>nous contacter</Link> pour que l’on puisse vous
           rediriger vers la bonne information.
-        </div>
+        </p>
         <Link asButton href='/'>
-          Page d'acceuil
+          Page d'accueil
         </Link>
       </Block>
     </div>
