97cbe7853bcb05193ecba64100141f840c64fdea
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-03-17 17:43:38 +0100

    feat: replace all itineraire iframes

--- app/(iframes)/iframes/transport/itineraire/page.tsx
+++ app/(iframes)/iframes/transport/itineraire/page.tsx
@@ -1,11 +1,12 @@
-import TransportIFramePage from 'src/views/TransportIFramePage'
 import { Category as CategoryType } from 'types/category'
 import { categories } from 'data/categories'
+import Category from 'components/outils/Category'
+import TransportSimulator from 'components/outils/TransportSimulator'
 
 const category = categories.find((category) => category.slug === 'transport') as CategoryType
 
 const page = () => {
-  return <TransportIFramePage category={category} />
+  return <Category category={category} simulator={<TransportSimulator />} noBottomBorders />
 }
 
 export default page
