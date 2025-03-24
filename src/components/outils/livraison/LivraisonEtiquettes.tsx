74eca72fc45e94d475203b8817f895f6e1dec8d6
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-03-24 21:22:51 +0100

    feat: allow sharing of etiquettes (#783)

--- src/components/outils/livraison/LivraisonEtiquettes.tsx
+++ src/components/outils/livraison/LivraisonEtiquettes.tsx
@@ -10,51 +10,53 @@ const LivraisonEtiquettes = () => {
 
   return (
     <div className={styles.etiquettes}>
-      <Shareable
-        slug='etiquette-statique'
-        tracking='Étiquette statique'
-        overScreens={overScreens.static}
-        secondary={
-          <div className={styles.secondary}>
-            <h3 className={styles.secondaryTitle}>Étiquette livraison statique</h3>
-            <div className={styles.secondarySeparator} />
-            <div className={styles.secondaryInfo}>
-              <div className={styles.icon}>
-                <Image src='/images/icn-next-actions-gray.svg' width={24} height={24} alt='' />
+      <div id='etiquette'>
+        <Shareable
+          slug='etiquette-statique'
+          tracking='Étiquette statique'
+          overScreens={overScreens.static}
+          secondary={
+            <div className={styles.secondary}>
+              <h3 className={styles.secondaryTitle}>Étiquette livraison statique</h3>
+              <div className={styles.secondarySeparator} />
+              <div className={styles.secondaryInfo}>
+                <div className={styles.icon}>
+                  <Image src='/images/icn-next-actions-gray.svg' width={24} height={24} alt='' />
+                </div>
+                <p>
+                  L’étiquette livraison offre une comparaison rapide des ordres de grandeur des scénarios d’achat, basée
+                  sur un colis moyen et des distances moyennes.
+                </p>
               </div>
-              <p>
-                L’étiquette livraison offre une comparaison rapide des ordres de grandeur des scénarios d’achat, basée
-                sur un colis moyen et des distances moyennes.
-              </p>
             </div>
-          </div>
-        }
-        withoutShare>
-        <LivraisonEtiquette id='static' />
-      </Shareable>
+          }>
+          <LivraisonEtiquette id='static' />
+        </Shareable>
+      </div>
       <div className={styles.separator} />
-      <Shareable
-        slug='etiquette-animee'
-        tracking='Étiquette animée'
-        overScreens={overScreens.animated}
-        secondary={
-          <div className={styles.secondary}>
-            <h3 className={styles.secondaryTitle}>Étiquette livraison animée</h3>
-            <div className={styles.secondarySeparator} />
-            <div className={styles.secondaryInfo}>
-              <div className={styles.icon}>
-                <Image src='/images/icn-next-actions-gray.svg' width={24} height={24} alt='' />
+      <div id='etiquette-animee'>
+        <Shareable
+          slug='etiquette-animee'
+          tracking='Étiquette animée'
+          overScreens={overScreens.animated}
+          secondary={
+            <div className={styles.secondary}>
+              <h3 className={styles.secondaryTitle}>Étiquette livraison animée</h3>
+              <div className={styles.secondarySeparator} />
+              <div className={styles.secondaryInfo}>
+                <div className={styles.icon}>
+                  <Image src='/images/icn-next-actions-gray.svg' width={24} height={24} alt='' />
+                </div>
+                <p>
+                  L’étiquette animée présente les mêmes contenus que l’étiquette standard, dans un format plus compact
+                  où chaque scénario défile toutes les 4 secondes.
+                </p>
               </div>
-              <p>
-                L’étiquette animée présente les mêmes contenus que l’étiquette standard, dans un format plus compact où
-                chaque scénario défile toutes les 4 secondes.
-              </p>
             </div>
-          </div>
-        }
-        withoutShare>
-        <LivraisonEtiquette animated id='animated' />
-      </Shareable>
+          }>
+          <LivraisonEtiquette animated id='animated' />
+        </Shareable>
+      </div>
     </div>
   )
 }
--- src/components/shareable/Actions.tsx
+++ src/components/shareable/Actions.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
-import React from 'react'
 import { track } from 'utils/matomo'
 import GhostButton from 'components/base/GhostButton'
 import CodeSSlashIcon from 'components/base/icons/code-s-slash'
@@ -13,27 +12,23 @@ const Actions = ({
   onClick,
   tracking,
   withoutIntegration,
-  withoutShare,
 }: {
   onClick: (value: 'partager' | 'integrer' | 'telecharger') => void
   tracking: string
   withoutIntegration?: boolean
-  withoutShare?: boolean
 }) => {
   const t = useTranslations('overscreen')
   return (
     <div className={styles.buttons}>
-      {!withoutShare && (
-        <GhostButton
-          data-testid='header-share-button'
-          icon={<SendPlaneIcon />}
-          onClick={() => {
-            onClick('partager')
-            track(tracking, 'Partager', `${tracking.replace(/ /g, '_').toLowerCase()}_partager`)
-          }}>
-          {t('share')}
-        </GhostButton>
-      )}
+      <GhostButton
+        data-testid='header-share-button'
+        icon={<SendPlaneIcon />}
+        onClick={() => {
+          onClick('partager')
+          track(tracking, 'Partager', `${tracking.replace(/ /g, '_').toLowerCase()}_partager`)
+        }}>
+        {t('share')}
+      </GhostButton>
       {!withoutIntegration && (
         <GhostButton
           data-testid='header-integrate-button'
