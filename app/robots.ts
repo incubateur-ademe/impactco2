3bdd173941ddcbcaf6a20ef2df593f7b6907a6ab
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-06-24 08:56:45 +0200

    chore: remove robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,11 +7,13 @@ export default function robots(): MetadataRoute.Robots {
           userAgent: '*',
           allow: '/',
         },
+        sitemap: 'https://impactco2.fr/sitemap.xml',
       }
     : {
         rules: {
           userAgent: '*',
           disallow: '/',
         },
+        sitemap: 'https://impactco2.fr/sitemap.xml',
       }
 }
