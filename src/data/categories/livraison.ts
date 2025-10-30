f13136ef2bae576708d934da0f7e9830abcc3e5f
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-10-30 10:30:09 +0100

    feat: custom livraison etiquette

--- src/data/categories/livraison.ts
+++ src/data/categories/livraison.ts
@@ -136,4 +136,415 @@ export const livraison = [
       },
     ],
   },
+  {
+    id: 8,
+    slug: 'magasin2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 0.25929,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 9,
+    slug: 'magasindouce2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 0.25929,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+    ],
+  },
+  {
+    id: 10,
+    slug: 'pointrelais2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 0.35271,
+      },
+      {
+        id: 55,
+        value: 0.01785,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+      {
+        id: 56,
+        value: 1.526,
+      },
+    ],
+  },
+  {
+    id: 11,
+    slug: 'pointrelaisdouce2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 0.35271,
+      },
+      {
+        id: 55,
+        value: 0.01785,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+    ],
+  },
+  {
+    id: 12,
+    slug: 'clickcollect2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 0.35271,
+      },
+      {
+        id: 55,
+        value: 0.25929,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 13,
+    slug: 'clickcollectdouce2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 0.35271,
+      },
+      {
+        id: 55,
+        value: 0.25929,
+      },
+      {
+        id: 33,
+        value: 0.57111,
+      },
+    ],
+  },
+  {
+    id: 14,
+    slug: 'livraisondomicile2kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 0.35271,
+      },
+      {
+        id: 55,
+        value: 0.01632,
+      },
+      {
+        id: 33,
+        value: 0.70595,
+      },
+    ],
+  },
+  {
+    id: 15,
+    slug: 'magasin15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 2.65272,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 16,
+    slug: 'magasindouce15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 2.65272,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+    ],
+  },
+  {
+    id: 17,
+    slug: 'pointrelais15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 1.63706,
+      },
+      {
+        id: 55,
+        value: 0.23835,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+      {
+        id: 56,
+        value: 1.526,
+      },
+    ],
+  },
+  {
+    id: 18,
+    slug: 'pointrelaisdouce15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 1.63706,
+      },
+      {
+        id: 55,
+        value: 0.23835,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+    ],
+  },
+  {
+    id: 19,
+    slug: 'clickcollect15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 1.63706,
+      },
+      {
+        id: 55,
+        value: 2.65272,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 20,
+    slug: 'clickcollectdouce15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 1.63706,
+      },
+      {
+        id: 55,
+        value: 2.65272,
+      },
+      {
+        id: 33,
+        value: 4.28335,
+      },
+    ],
+  },
+  {
+    id: 21,
+    slug: 'livraisondomicile15kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 1.63706,
+      },
+      {
+        id: 55,
+        value: 0.22298,
+      },
+      {
+        id: 33,
+        value: 5.29464,
+      },
+    ],
+  },
+  {
+    id: 22,
+    slug: 'magasin30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 5.30001,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 23,
+    slug: 'magasindouce30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 55,
+        value: 5.30001,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+    ],
+  },
+  {
+    id: 24,
+    slug: 'pointrelais30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 2.59865,
+      },
+      {
+        id: 55,
+        value: 0.47127,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+      {
+        id: 56,
+        value: 1.526,
+      },
+    ],
+  },
+  {
+    id: 25,
+    slug: 'pointrelaisdouce30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 2.59865,
+      },
+      {
+        id: 55,
+        value: 0.47127,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+    ],
+  },
+  {
+    id: 26,
+    slug: 'clickcollect30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 2.59865,
+      },
+      {
+        id: 55,
+        value: 5.30001,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+      {
+        id: 57,
+        value: 6.54,
+      },
+    ],
+  },
+  {
+    id: 27,
+    slug: 'clickcollectdouce30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 2.59865,
+      },
+      {
+        id: 55,
+        value: 5.30001,
+      },
+      {
+        id: 33,
+        value: 8.56669,
+      },
+    ],
+  },
+  {
+    id: 28,
+    slug: 'livraisondomicile30kg',
+    category: 12,
+    ecv: [
+      {
+        id: 54,
+        value: 2.59865,
+      },
+      {
+        id: 55,
+        value: 0.44052,
+      },
+      {
+        id: 33,
+        value: 10.58928,
+      },
+    ],
+  },
 ]
