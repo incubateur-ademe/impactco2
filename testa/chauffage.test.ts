f0e502f064422bcfb6891b9f357d07e1ecc3b0cf
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-01-12 10:12:34 +0100

    fix electricite

--- testa/chauffage.test.ts
+++ testa/chauffage.test.ts
@@ -12,7 +12,7 @@ describe('chauffage', () => {
       data: [
         { ecv: 1123.735932762777, name: 'Chaudière à gaz', slug: 'chauffagegaz' },
         { ecv: 2203.184061208831, name: 'Chaudière au fioul', slug: 'chauffagefioul' },
-        { ecv: 151.81056915609627, name: 'Chaudière électrique', slug: 'chauffageelectrique' },
+        { ecv: 151.81056915609627, name: 'Électricité', slug: 'chauffageelectrique' },
         { ecv: 102.9031975493033, name: 'Pompe à chaleur', slug: 'pompeachaleur' },
         { ecv: 372.7681482247014, name: 'Poêle à granulés', slug: 'poeleagranule' },
         { ecv: 535.8542130730084, name: 'Poêle à bûches', slug: 'poeleabois' },
@@ -42,7 +42,7 @@ describe('chauffage', () => {
       data: [
         { ecv: 1123.735932762777, name: 'Gas boiler', slug: 'chauffagegaz' },
         { ecv: 2203.184061208831, name: 'Oil boiler', slug: 'chauffagefioul' },
-        { ecv: 151.81056915609627, name: 'Electric boiler', slug: 'chauffageelectrique' },
+        { ecv: 151.81056915609627, name: 'Electricity', slug: 'chauffageelectrique' },
         { ecv: 102.9031975493033, name: 'Heat pump', slug: 'pompeachaleur' },
         { ecv: 372.7681482247014, name: 'Pellet stove', slug: 'poeleagranule' },
         { ecv: 535.8542130730084, name: 'Wood stove', slug: 'poeleabois' },
@@ -87,7 +87,7 @@ describe('chauffage', () => {
       data: [
         { ecv: 1783.7078297821858, name: 'Chaudière à gaz', slug: 'chauffagegaz' },
         { ecv: 3497.1175574743347, name: 'Chaudière au fioul', slug: 'chauffagefioul' },
-        { ecv: 240.969157390629, name: 'Chaudière électrique', slug: 'chauffageelectrique' },
+        { ecv: 240.969157390629, name: 'Électricité', slug: 'chauffageelectrique' },
         { ecv: 163.33840880841795, name: 'Pompe à chaleur', slug: 'pompeachaleur' },
         { ecv: 591.695473372542, name: 'Poêle à granulés', slug: 'poeleagranule' },
         { ecv: 850.5622429730291, name: 'Poêle à bûches', slug: 'poeleabois' },
