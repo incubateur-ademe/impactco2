f884e0b166647e98d969e9adb57628db9d299bd3
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-02-17 13:34:52 +0100

    feat: update detecteur exemples

--- src/components/outils/DetectorInformation.tsx
+++ src/components/outils/DetectorInformation.tsx
@@ -17,57 +17,50 @@ const DetectorInformation = () => {
         <b>En fonction du poids identifié</b>, l’algorithme du Détecteur propose une sélection d’équivalents adaptés,
         permettant d’illustrer des quantités carbone aussi bien en grammes de CO2e qu’en millions de tonnes de CO2e.
       </p>
-      <p className={styles.title}>Pourquoi utiliser le Détecteur ?</p>
-      <ul className={styles.list}>
-        <li>
-          Pour faciliter <b>la compréhension des données carbone</b> sur une page web grâce aux bons ordres de grandeur.
-        </li>
-        <li>
-          Pour <b>sensibiliser les publics aux enjeux carbone</b>, en rendant visible l'impact réel des activités
-          humaines à travers leurs émissions.
-        </li>
-      </ul>
       <p>
         L’outil s’adresse principalement aux <b>médias, entreprises, collectivités</b> ou <b>associations</b> qui
         communiquent régulièrement des données carbone auprès de leur audience. Il a déjà été adopté par plusieurs
         acteurs comme{' '}
         <Link
+          href='https://www.franceinfo.fr/environnement/transparence-comment-se-representer-les-emissions-de-co2-franceinfo-integre-le-comparateur-carbone-de-l-ademe-dans-ses-articles_7736326.html'
+          target='_blank'
+          rel='noreferrer noopener'>
+          Franceinfo
+        </Link>
+        ,{' '}
+        <Link
           href='https://www.linfodurable.fr/climat/detecteur-carbone-id-linfo-durable-premier-media-se-doter-du-nouvel-outil-de-lademe-44711'
           target='_blank'
-          rel='noreferer noopener'>
+          rel='noreferrer noopener'>
           ID, L’Info Durable
         </Link>
         ,{' '}
         <Link
           href='https://www.lanouvellerepublique.fr/a-la-une/les-emissions-de-gaz-a-effet-de-serre-baissent-en-france'
           target='_blank'
-          rel='noreferer noopener'>
+          rel='noreferrer noopener'>
           La Nouvelle République
         </Link>
         ,{' '}
         <Link
           href='https://greenly.earth/blog/secteurs/empreinte-carbone-vol-en-avion'
           target='_blank'
-          rel='noreferer noopener'>
+          rel='noreferrer noopener'>
           Greenly
         </Link>
-        ,{' '}
-        <Link href='https://www.homeexchange.fr/blog/bilan-carbone-2022/' target='_blank' rel='noreferer noopener'>
+        ou encore{' '}
+        <Link href='https://www.homeexchange.fr/blog/bilan-carbone-2022/' target='_blank' rel='noreferrer noopener'>
           HomeExchange
         </Link>{' '}
-        ou encore{' '}
-        <Link
-          href='https://www.gobilab.com/blogs/blog/pourquoi-et-comment-orienter-son-entreprise-vers-le-reutilisable'
-          target='_blank'
-          rel='noreferer noopener'>
-          Gobi
-        </Link>
         .
       </p>
-      <div>
+      <div className={styles.imageContainer}>
         <Image src='/images/detecteur-co2.webp' alt='' width={680} height={208} />
         <div className={styles.legend}>
-          <p>Greenly a utilisé le Détecteur pour illustrer les quantités carbone dans ses articles de blog.</p>
+          <p>
+            Franceinfo utilise le Détecteur pour illustrer les quantités carbone dans ses articles de la rubrique
+            Environnement.
+          </p>
         </div>
       </div>
     </>
