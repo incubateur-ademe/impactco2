fe8d76800bd86352769258bc3cf04004f85e5c93
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2026-04-13 17:21:15 +0200

    feat: add integrabook for visual integration testing (#878)

---
+++ integrabook/config.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration de l'intégrabook.
+ * Modifiez ce fichier pour cibler votre environnement.
+ */
+const INTEGRABOOK_CONFIG = {
+  baseUrl: 'https://impactco2.fr',
+  theme: 'default', // 'default' | 'night'
+  language: 'fr', // 'fr' | 'en' | 'es'
+}
